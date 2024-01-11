8553d6d9b2cd7b8fd4e5757319eb24067e24c65e
polebug <polebugfly@gmail.com>
2024-01-11 11:26:34 +0800

    feat(hub): upgrade rss3 staking contract

--- schema/node.go
+++ schema/node.go
@@ -1,17 +1,23 @@
 package schema
 
 import (
+	"math/big"
+
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/naturalselectionlabs/rss3-node/config"
 )
 
 type Node struct {
-	Address      common.Address `json:"address"`
-	Name         string         `json:"name"`
-	Description  string         `json:"description"`
-	Endpoint     string         `json:"-"`
-	TaxFraction  uint64         `json:"taxFraction"`
-	IsPublicGood bool           `json:"isPublicGood"`
-	Stream       *config.Stream `json:"-"`
-	Config       *config.Node   `json:"-"`
+	Address             common.Address `json:"address"`
+	Name                string         `json:"name"`
+	Description         string         `json:"description"`
+	TaxFraction         uint64         `json:"taxFraction"`
+	IsPublicGood        bool           `json:"isPublicGood"`
+	OperatingPoolTokens *big.Int       `json:"operatingPoolTokens"`
+	StakingPoolTokens   *big.Int       `json:"stakingPoolTokens"`
+	TotalShares         *big.Int       `json:"totalShares"`
+	SlashedTokens       *big.Int       `json:"slashedTokens"`
+	Endpoint            string         `json:"-"`
+	Stream              *config.Stream `json:"-"`
+	Config              *config.Node   `json:"-"`
 }
