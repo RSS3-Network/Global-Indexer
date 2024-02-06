8871f83f61f625801858a09a0565fc3c7cdb3373
KallyDev <kallydev@rss3.io>
2024-02-06 14:52:51 +0800

    chore: update L1 contract addresses (#52)

--- contract/l1/contract.go
+++ contract/l1/contract.go
@@ -8,10 +8,10 @@ import (
 //go:generate go run --mod=mod github.com/ethereum/go-ethereum/cmd/abigen@v1.13.5 --abi ./abi/L1StandardBridge.abi --pkg l1 --type L1StandardBridge --out contract_l1_standard_bridge.go
 
 var (
-	AddressL1CrossDomainMessengerProxy = common.HexToAddress("0x17C635E784B0f098Ab57A39d6dDeA0C786A3AfC1") // https://sepolia.etherscan.io/address/0x17C635E784B0f098Ab57A39d6dDeA0C786A3AfC1
-	AddressL1StandardBridgeProxy       = common.HexToAddress("0xc575bd904D16a433624db98D01d5AbD5c92D0F38") // https://sepolia.etherscan.io/address/0xc575bd904D16a433624db98D01d5AbD5c92D0F38
-	AddressOptimismPortalProxy         = common.HexToAddress("0xb58f3f17Ef3fAF6cd1C4Fa87b6e15A97B653993E") // https://sepolia.etherscan.io/address/0xb58f3f17Ef3fAF6cd1C4Fa87b6e15A97B653993E
-	AddressGovernanceTokenProxy        = common.HexToAddress("0x568F64582A377ea52d0067c4E430B9aE22A60473") // https://sepolia.etherscan.io/address/0x568F64582A377ea52d0067c4E430B9aE22A60473
+	AddressL1CrossDomainMessengerProxy = common.HexToAddress("0xb0496292B1A82B284898fA946BE34BdB43B2aDee") // https://sepolia.etherscan.io/address/0xb0496292B1A82B284898fA946BE34BdB43B2aDee
+	AddressL1StandardBridgeProxy       = common.HexToAddress("0x30110496b378F5AaB1438E9cf48421f9173841A1") // https://sepolia.etherscan.io/address/0x30110496b378F5AaB1438E9cf48421f9173841A1
+	AddressOptimismPortalProxy         = common.HexToAddress("0xB5143A98b600559398A12cC5F4ec8B9F97A7aD63") // https://sepolia.etherscan.io/address/0xB5143A98b600559398A12cC5F4ec8B9F97A7aD63
+	AddressGovernanceTokenProxy        = common.HexToAddress("0x3Ef1D5be1E2Ce46c583a0c8e511f015706A0ab23") // https://sepolia.etherscan.io/address/0x3Ef1D5be1E2Ce46c583a0c8e511f015706A0ab23
 )
 
 var (
