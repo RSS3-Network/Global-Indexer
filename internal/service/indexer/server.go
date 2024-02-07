f00afb0aa5eae410596e13d622ed2c7e49b1754c
polebug <polebugfly@gmail.com>
2024-02-07 15:41:02 +0800

    chore

--- internal/service/indexer/server.go
+++ internal/service/indexer/server.go
@@ -2,10 +2,10 @@ package indexer
 
 import (
 	"context"
-	"github.com/naturalselectionlabs/rss3-global-indexer/internal/service/indexer/l1"
 
 	"github.com/naturalselectionlabs/rss3-global-indexer/internal/config"
 	"github.com/naturalselectionlabs/rss3-global-indexer/internal/database"
+	"github.com/naturalselectionlabs/rss3-global-indexer/internal/service/indexer/l1"
 	"github.com/naturalselectionlabs/rss3-global-indexer/internal/service/indexer/l2"
 	"github.com/sourcegraph/conc/pool"
 )
