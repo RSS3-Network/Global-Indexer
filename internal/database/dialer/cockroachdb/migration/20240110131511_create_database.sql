b6fdfe7f7a8411f444eceacf6ebeaaf0bd35a2f1
polebug <polebugfly@gmail.com>
2024-02-08 09:28:23 +0800

    chore: merge migration sql

--- internal/database/dialer/cockroachdb/migration/20240110131511_create_database.sql
+++ internal/database/dialer/cockroachdb/migration/20240110131511_create_database.sql
@@ -2,13 +2,15 @@
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS "node_info"
 (
-    "address"        bytea       NOT NULL,
-    "endpoint"       text        NOT NULL,
-    "is_public_good" bool        NOT NULL,
-    "stream"         json        NOT NULL,
-    "config"         json        NOT NULL,
-    "created_at"     timestamptz NOT NULL DEFAULT now(),
-    "updated_at"     timestamptz NOT NULL DEFAULT now(),
+    "address"                  bytea       NOT NULL,
+    "endpoint"                 text        NOT NULL,
+    "is_public_good"           bool        NOT NULL,
+    "stream"                   json        NOT NULL,
+    "config"                   json        NOT NULL,
+    "status"                   TEXT        NOT NULL DEFAULT 'offline',
+    "last_heartbeat_timestamp" timestamptz,
+    "created_at"               timestamptz NOT NULL DEFAULT now(),
+    "updated_at"               timestamptz NOT NULL DEFAULT now(),
 
     CONSTRAINT "pk_indexes" PRIMARY KEY ("address")
 );
@@ -16,6 +18,8 @@ CREATE TABLE IF NOT EXISTS "node_info"
 CREATE UNIQUE INDEX IF NOT EXISTS "idx_endpoint_unique" ON "node_info" ("endpoint");
 CREATE INDEX IF NOT EXISTS "idx_is_public" ON "node_info" ("is_public_good", "created_at" DESC);
 CREATE INDEX IF NOT EXISTS "idx_created_at" ON "node_info" ("address", "created_at" DESC);
+CREATE INDEX IF NOT EXISTS "idx_status" ON "node_info" ("status");
+CREATE INDEX IF NOT EXISTS "idx_last_heartbeat_timestamp" ON "node_info" ("last_heartbeat_timestamp");
 -- +goose StatementEnd
 
 -- +goose Down
