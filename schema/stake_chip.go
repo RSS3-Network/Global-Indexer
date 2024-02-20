5acc05092e5649f9471cd12a429178e304732941
KallyDev <kallydev@rss3.io>
2024-02-20 22:14:09 +0800

    feat(indexer): index chips token metadata

--- schema/stake_chip.go
+++ schema/stake_chip.go
@@ -1,6 +1,7 @@
 package schema
 
 import (
+	"encoding/json"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -20,16 +21,17 @@ type StakeChipTransformer interface {
 }
 
 type StakeChip struct {
-	ID             *big.Int       `json:"id"`
-	Owner          common.Address `json:"owner"`
-	Node           common.Address `json:"node"`
-	BlockNumber    *big.Int       `json:"blockNumber"`
-	BlockTimestamp uint64         `json:"blockTimestamp"`
+	ID             *big.Int        `json:"id"`
+	Owner          common.Address  `json:"owner"`
+	Node           common.Address  `json:"node"`
+	Metadata       json.RawMessage `json:"metadata"`
+	BlockNumber    *big.Int        `json:"blockNumber"`
+	BlockTimestamp uint64          `json:"blockTimestamp"`
 }
 
 type StakeChipsQuery struct {
-	Cursor *common.Address
-	Direct bool
+	Cursor *common.Address `query:"cursor"`
+	Direct bool            `query:"direct"`
 	ID     *big.Int        `query:"id"`
 	Owner  *common.Address `query:"owner"`
 	Node   *common.Address `query:"node"`
