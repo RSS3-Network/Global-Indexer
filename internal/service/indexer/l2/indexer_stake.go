4d19a6d031764e366ee59affabc81a4f09540a49
KallyDev <kallydev@rss3.io>
2024-03-08 17:02:27 +0800

    feat(indexer): add `hide tax rate` field

--- internal/service/indexer/l2/indexer_stake.go
+++ internal/service/indexer/l2/indexer_stake.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"math/big"
 	"strings"
@@ -15,6 +16,7 @@ import (
 	"github.com/naturalselectionlabs/rss3-global-indexer/contract/l2"
 	"github.com/naturalselectionlabs/rss3-global-indexer/internal/database"
 	"github.com/naturalselectionlabs/rss3-global-indexer/schema"
+	"github.com/redis/go-redis/v9"
 	"github.com/samber/lo"
 	"github.com/shopspring/decimal"
 	"go.uber.org/zap"
@@ -417,6 +419,11 @@ func (s *server) indexStakingNodeCreated(ctx context.Context, header *types.Head
 		Status:             schema.NodeStatusRegistered,
 	}
 
+	// Get from redis if the tax rate of the node needs to be hidden.
+	if err := s.cacheClient.Get(ctx, s.buildNodeHideTaxRateKey(node.Address), &node.HideTaxRate); err != nil && !errors.Is(err, redis.Nil) {
+		return fmt.Errorf("get hide tax rate: %w", err)
+	}
+
 	// save node avatar
 	avatar, err := s.contractStaking.GetNodeAvatar(&bind.CallOpts{BlockNumber: header.Number}, event.NodeAddr)
 	if err != nil {
@@ -442,4 +449,8 @@ func (s *server) indexStakingNodeCreated(ctx context.Context, header *types.Head
 	}
 
 	return nil
+}
+
+func (s *server) buildNodeHideTaxRateKey(address common.Address) string {
+	return fmt.Sprintf("node::%s::hideTaxRate", strings.ToLower(address.String()))
 }
