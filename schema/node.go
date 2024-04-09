0af280f4f1a9759404a89446643470060644d293
brucexc <brucexc@rss3.io>
2024-04-09 18:28:37 -0400

    refactor: extract node_stat and node_indexer

--- schema/node.go
+++ schema/node.go
@@ -2,12 +2,10 @@ package schema
 
 import (
 	"encoding/json"
-	"math/big"
-	"time"
-
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/naturalselectionlabs/rss3-global-indexer/contract/l2"
 	"github.com/shopspring/decimal"
+	"math/big"
 )
 
 type Node struct {
@@ -53,41 +51,6 @@ const (
 	NodeStatusOffline                      // offline
 	NodeStatusExited                       // exiting
 )
-
-type Stat struct {
-	Address              common.Address `json:"address"`
-	Endpoint             string         `json:"-"`
-	Points               float64        `json:"points"`
-	IsPublicGood         bool           `json:"isPublicGood"`
-	IsFullNode           bool           `json:"isFullNode"`
-	IsRssNode            bool           `json:"isRssNode"`
-	Staking              float64        `json:"staking"`
-	Epoch                int64          `json:"epoch"`
-	TotalRequest         int64          `json:"totalRequest"`
-	EpochRequest         int64          `json:"epochRequest"`
-	EpochInvalidRequest  int64          `json:"epochInvalidRequest"`
-	DecentralizedNetwork int            `json:"decentralizedNetwork"`
-	FederatedNetwork     int            `json:"federatedNetwork"`
-	Indexer              int            `json:"indexer"`
-	ResetAt              time.Time      `json:"resetAt"`
-}
-
-type StatQuery struct {
-	Address      *common.Address  `query:"address" form:"address,omitempty"`
-	AddressList  []common.Address `query:"addressList" form:"addressList,omitempty"`
-	IsFullNode   *bool            `query:"isFullNode" form:"isFullNode,omitempty"`
-	IsRssNode    *bool            `query:"isRssNode" form:"isRssNode,omitempty"`
-	PointsOrder  *string          `query:"pointsOrder" form:"pointsOrder,omitempty"`
-	ValidRequest *int             `query:"validRequest" form:"validRequest,omitempty"`
-	Limit        *int             `query:"limit" form:"limit,omitempty"`
-	Cursor       *string          `query:"cursor" form:"cursor,omitempty"`
-}
-
-type Indexer struct {
-	Address common.Address `json:"address"`
-	Network string         `json:"network"`
-	Worker  string         `json:"worker"`
-}
 
 type BatchUpdateNode struct {
 	Address          common.Address
