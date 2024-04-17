4c28ef00fb06733ad0316331472efec95baee213
Nya Candy <dev@candinya.com>
2024-04-17 11:18:41 +0800

    chore: tidy mods

--- cmd/tools.go
+++
@@ -1,6 +0,0 @@
-package main
-
-// https://go.dev/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
-// tracking tools dependencies
-
-import _ "ariga.io/atlas-provider-gorm/gormschema" // For Atlas to generate schemas from gorm models
