ff6470a5be91a631cb1a59fa981581385d0a5b0c
brucexc <brucexc@rss3.io>
2024-04-18 13:20:10 -0400

    refactor: matchLightNodes

--- internal/service/hub/handler/dsl/distributor/match.go
+++ internal/service/hub/handler/dsl/distributor/match.go
@@ -2,10 +2,12 @@ package distributor
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/rss3-network/global-indexer/internal/service/hub/handler/dsl/model"
 	"github.com/rss3-network/global-indexer/internal/service/hub/model/dsl"
+	"github.com/rss3-network/global-indexer/schema"
 	"github.com/rss3-network/protocol-go/schema/filter"
 	"github.com/samber/lo"
 )
@@ -13,31 +15,57 @@ import (
 // matchLightNodes matches light nodes based on the given account activities request,
 // and returns the addresses of light nodes that match the request.
 func (d *Distributor) matchLightNodes(ctx context.Context, request dsl.AccountActivitiesRequest) ([]common.Address, error) {
-	tagWorkers := make(map[string]struct{})
-	platformWorkers := make(map[string]struct{})
+	// Find network nodes that match the network requests.
+	networks, err := getNetworks(request.Network)
+	if err != nil {
+		return nil, err
+	}
 
-	// check network
-	for i, network := range request.Network {
+	// Find tag workers that match the tag requests.
+	tagWorkers, err := getTagWorkers(request.Tag)
+	if err != nil {
+		return nil, err
+	}
+
+	// Find platform workers that match the platform requests.
+	platformWorkers, err := getPlatformWorkers(request.Platform)
+	if err != nil {
+		return nil, err
+	}
+
+	// Find nodes that match the tag workers, platform workers, and networks.
+	return d.findQualifiedNodes(ctx, tagWorkers, platformWorkers, networks)
+}
+
+type WorkerSet map[string]struct{}
+
+// getNetworks returns a slice of networks based on the given network names.
+func getNetworks(networks []string) ([]string, error) {
+	for i, network := range networks {
 		nid, err := filter.NetworkString(network)
 		if err != nil {
 			return nil, err
 		}
 
-		request.Network[i] = nid.String()
+		networks[i] = nid.String()
 	}
 
-	// check tag
-	for _, tag := range request.Tag {
-		tid, err := filter.TagString(tag)
+	return networks, nil
+}
 
+// getTagWorkers returns a set of workers based on the given tags.
+func getTagWorkers(tags []string) (WorkerSet, error) {
+	tagWorkers := make(WorkerSet)
+
+	for _, tag := range tags {
+		tid, err := filter.TagString(tag)
 		if err != nil {
 			return nil, err
 		}
 
 		tagWorker, exists := model.TagToWorkersMap[tid]
-
 		if !exists {
-			return nil, err
+			return nil, fmt.Errorf("no workers found for tag: %s", tid)
 		}
 
 		for _, worker := range tagWorker {
@@ -45,249 +73,273 @@ func (d *Distributor) matchLightNodes(ctx context.Context, request dsl.AccountAc
 		}
 	}
 
-	// check platform
-	for _, platform := range request.Platform {
-		pid, err := filter.PlatformString(platform)
+	return tagWorkers, nil
+}
 
+// getPlatformWorkers returns a set of workers based on the given platforms.
+func getPlatformWorkers(platforms []string) (WorkerSet, error) {
+	platformWorkers := make(WorkerSet)
+
+	for _, platform := range platforms {
+		pid, err := filter.PlatformString(platform)
 		if err != nil {
 			return nil, err
 		}
 
 		platformWorker, exists := model.PlatformToWorkerMap[pid]
-
 		if !exists {
-			return nil, err
+			return nil, fmt.Errorf("no worker found for platform: %s", pid)
 		}
 
 		platformWorkers[platformWorker] = struct{}{}
 	}
 
-	var (
-		nodes        []common.Address
-		err          error
-		workers      []string
-		needsWorker  = len(tagWorkers) > 0 || len(platformWorkers) > 0
-		needsNetwork = len(request.Network) > 0
-	)
+	return platformWorkers, nil
+}
 
-	if len(tagWorkers) > 0 && len(platformWorkers) > 0 {
-		workers = findCommonStr(lo.Keys(tagWorkers), lo.Keys(platformWorkers))
-	} else if len(tagWorkers) > 0 {
-		workers = lo.Keys(tagWorkers)
-	} else if len(platformWorkers) > 0 {
-		workers = lo.Keys(platformWorkers)
+// findQualifiedNodes finds nodes that match the given tag workers, platform workers, and networks.
+func (d *Distributor) findQualifiedNodes(ctx context.Context, tagWorkers, platformWorkers WorkerSet, networks []string) ([]common.Address, error) {
+	workers := combineWorkers(tagWorkers, platformWorkers)
+	// If no common workers are found between tag workers and platform workers,
+	// it indicates that tags and platforms are not compatible.
+	if len(workers) == 0 && (len(tagWorkers) > 0 || len(platformWorkers) > 0) {
+		return nil, fmt.Errorf("no workers found for tags and platforms")
 	}
 
+	var (
+		nodes []common.Address
+		err   error
+	)
+
 	switch {
-	case !needsWorker && needsNetwork:
-		nodes, err = d.matchNetwork(ctx, request.Network)
-	case needsWorker && !needsNetwork:
+	case len(workers) > 0 && len(networks) > 0:
+		nodes, err = d.matchWorkerAndNetwork(ctx, workers, networks)
+	case len(workers) > 0:
 		nodes, err = d.matchWorker(ctx, workers)
-	case needsWorker && needsNetwork:
-		nodes, err = d.matchWorkerAndNetwork(ctx, workers, request.Network)
+	case len(networks) > 0:
+		nodes, err = d.matchNetwork(ctx, networks)
 	default:
 	}
 
-	if err != nil {
-		return nil, err
+	return nodes, err
+}
+
+// combineWorkers combines tag workers and platform workers.
+func combineWorkers(tagWorkers, platformWorkers WorkerSet) []string {
+	if len(tagWorkers) == 0 {
+		return lo.Keys(platformWorkers)
 	}
 
-	return nodes, nil
+	if len(platformWorkers) == 0 {
+		return lo.Keys(tagWorkers)
+	}
+
+	// Find common workers between tag workers and platform workers.
+	return findCommonElements(lo.Keys(tagWorkers), lo.Keys(platformWorkers))
 }
 
 // matchNetwork matches nodes based on the given network requests,
 // and returns the addresses of nodes that match the requests.
-func (d *Distributor) matchNetwork(ctx context.Context, requestNetworks []string) ([]common.Address, error) {
-	nodes := make([]common.Address, 0)
-
-	indexers, err := d.databaseClient.FindNodeIndexers(ctx, nil, requestNetworks, nil)
-
+func (d *Distributor) matchNetwork(ctx context.Context, networks []string) ([]common.Address, error) {
+	// Find all indexers that match the networks.
+	indexers, err := d.databaseClient.FindNodeIndexers(ctx, nil, networks, nil)
 	if err != nil {
 		return nil, err
 	}
 
-	nodeNetworkToWorkerMap := make(map[common.Address]map[string][]string)
+	// Generate a map of node addresses to network workers.
+	nodeNetworkWorkersMap := generateNodeNetworkWorkersMap(indexers)
+	// Filter nodes that match the network requests.
+	return filterMatchingNetworkNodes(nodeNetworkWorkersMap, networks), nil
+}
+
+type NetworkWorkersMap struct {
+	Workers map[string][]string
+}
+
+// generateNodeNetworkWorkersMap generates a map of node addresses to network workers.
+func generateNodeNetworkWorkersMap(indexers []*schema.Indexer) map[common.Address]NetworkWorkersMap {
+	nodeNetworkWorkersMap := make(map[common.Address]NetworkWorkersMap)
 
 	for _, indexer := range indexers {
-		if _, exists := nodeNetworkToWorkerMap[indexer.Address]; !exists {
-			nodeNetworkToWorkerMap[indexer.Address] = make(map[string][]string)
+		if _, exists := nodeNetworkWorkersMap[indexer.Address]; !exists {
+			nodeNetworkWorkersMap[indexer.Address] = NetworkWorkersMap{Workers: make(map[string][]string)}
 		}
 
-		nodeNetworkToWorkerMap[indexer.Address][indexer.Network] = append(nodeNetworkToWorkerMap[indexer.Address][indexer.Network], indexer.Worker)
+		networkWorkersMap := nodeNetworkWorkersMap[indexer.Address].Workers
+		networkWorkersMap[indexer.Network] = append(networkWorkersMap[indexer.Network], indexer.Worker)
 	}
 
-	for address, networkToWorkersMap := range nodeNetworkToWorkerMap {
-		if len(networkToWorkersMap) != len(requestNetworks) {
-			// number of networks not match
-			continue
-		}
+	return nodeNetworkWorkersMap
+}
 
-		flag := true
+// filterMatchingNetworkNodes filters nodes that match the network requests.
+func filterMatchingNetworkNodes(nodeNetworkWorkersMap map[common.Address]NetworkWorkersMap, requestNetworks []string) []common.Address {
+	var nodes []common.Address
 
-		// check every network workers
-		for network, workers := range networkToWorkersMap {
-			nid, _ := filter.NetworkString(network)
-
-			requiredWorkers := model.NetworkToWorkersMap[nid]
-
-			// number of workers not match
-			if len(requiredWorkers) != len(workers) {
-				flag = false
-
-				break
-			}
-
-			workerMap := make(map[string]struct{}, len(workers))
-
-			for _, worker := range workers {
-				workerMap[worker] = struct{}{}
-			}
-
-			// check every worker
-			for _, worker := range requiredWorkers {
-				if _, exists := workerMap[worker]; !exists {
-					flag = false
-
-					break
-				}
-			}
-		}
-
-		if flag {
+	for address, networkWorkersMap := range nodeNetworkWorkersMap {
+		if isValidNetworkNode(networkWorkersMap, requestNetworks) {
 			nodes = append(nodes, address)
 		}
 	}
 
-	return nodes, nil
+	return nodes
+}
+
+// isValidNetworkNode checks if the node matches the required networks.
+func isValidNetworkNode(networkWorkersMap NetworkWorkersMap, requestNetworks []string) bool {
+	// Check if the number of networks match the number of request networks.
+	if len(networkWorkersMap.Workers) != len(requestNetworks) {
+		return false
+	}
+
+	for network, workers := range networkWorkersMap.Workers {
+		nid, _ := filter.NetworkString(network)
+
+		// Check if the workers match the required workers for the network.
+		requiredWorkers := model.NetworkToWorkersMap[nid]
+		if !AreSliceElementsEqual(workers, requiredWorkers) {
+			return false
+		}
+	}
+
+	return true
+}
+
+type WorkerNetworksMap struct {
+	Networks map[string][]string
 }
 
 // matchWorker matches nodes based on the given worker names,
 // and returns the addresses of nodes that match the requests.
 func (d *Distributor) matchWorker(ctx context.Context, workers []string) ([]common.Address, error) {
-	nodes := make([]common.Address, 0)
-
 	indexers, err := d.databaseClient.FindNodeIndexers(ctx, nil, nil, workers)
+	if err != nil {
+		return nil, err
+	}
+
+	nodeWorkerNetworksMap := generateNodeWorkerNetworksMap(indexers)
+
+	return filterMatchingWorkerNodes(nodeWorkerNetworksMap, workers), nil
+}
+
+// generateNodeWorkerNetworksMap generates a map of node addresses to worker networks.
+func generateNodeWorkerNetworksMap(indexers []*schema.Indexer) map[common.Address]WorkerNetworksMap {
+	nodeWorkerNetworksMap := make(map[common.Address]WorkerNetworksMap)
+
+	for _, indexer := range indexers {
+		if _, exists := nodeWorkerNetworksMap[indexer.Address]; !exists {
+			nodeWorkerNetworksMap[indexer.Address] = WorkerNetworksMap{Networks: make(map[string][]string)}
+		}
+
+		workerNetworksMap := nodeWorkerNetworksMap[indexer.Address].Networks
+		workerNetworksMap[indexer.Worker] = append(workerNetworksMap[indexer.Worker], indexer.Network)
+	}
+
+	return nodeWorkerNetworksMap
+}
+
+// filterMatchingWorkerNodes filters nodes that match the worker requests.
+func filterMatchingWorkerNodes(nodeWorkerNetworksMap map[common.Address]WorkerNetworksMap, workers []string) []common.Address {
+	var nodes []common.Address
+
+	for address, workerNetworksMap := range nodeWorkerNetworksMap {
+		if isValidWorkerNode(workerNetworksMap, workers) {
+			nodes = append(nodes, address)
+		}
+	}
+
+	return nodes
+}
+
+// isValidWorkerNode checks if the node matches the required workers.
+func isValidWorkerNode(workerNetworksMap WorkerNetworksMap, workers []string) bool {
+	if len(workerNetworksMap.Networks) != len(workers) {
+		return false
+	}
+
+	for worker, networks := range workerNetworksMap.Networks {
+		wid, _ := filter.NameString(worker)
+
+		requiredNetworks := model.WorkerToNetworksMap[wid]
+		if !AreSliceElementsEqual(networks, requiredNetworks) {
+			return false
+		}
+	}
+
+	return true
+}
+
+// matchWorkerAndNetwork matches nodes based on both worker and network.
+func (d *Distributor) matchWorkerAndNetwork(ctx context.Context, workers, networks []string) ([]common.Address, error) {
+	indexers, err := d.databaseClient.FindNodeIndexers(ctx, nil, networks, workers)
 
 	if err != nil {
 		return nil, err
 	}
 
-	nodeWorkerToNetworksMap := make(map[common.Address]map[string][]string)
+	nodeWorkerNetworksMap := generateNodeWorkerNetworksMap(indexers)
 
-	for _, indexer := range indexers {
-		if _, exists := nodeWorkerToNetworksMap[indexer.Address]; !exists {
-			nodeWorkerToNetworksMap[indexer.Address] = make(map[string][]string)
-		}
+	return filterMatchingWorkerAndNetworkNodes(nodeWorkerNetworksMap, workers, networks), nil
+}
 
-		nodeWorkerToNetworksMap[indexer.Address][indexer.Worker] = append(nodeWorkerToNetworksMap[indexer.Address][indexer.Worker], indexer.Network)
-	}
+// filterMatchingWorkerAndNetworkNodes filters nodes that match the worker and network requests.
+func filterMatchingWorkerAndNetworkNodes(nodeWorkerNetworksMap map[common.Address]WorkerNetworksMap, workers, networks []string) []common.Address {
+	var nodes []common.Address
 
-	for address, workerToNetworksMap := range nodeWorkerToNetworksMap {
-		if len(nodeWorkerToNetworksMap) != len(workers) {
-			// number of workers not match
-			continue
-		}
-
-		flag := true
-
-		// check every worker networks
-		for worker, networks := range workerToNetworksMap {
-			wid, _ := filter.NameString(worker)
-
-			requiredNetworks := model.WorkerToNetworksMap[wid]
-
-			// number of networks not match
-			if len(requiredNetworks) != len(networks) {
-				flag = false
-
-				break
-			}
-
-			networkMap := make(map[string]struct{}, len(networks))
-
-			for _, network := range networks {
-				networkMap[network] = struct{}{}
-			}
-
-			// check every network
-			for _, network := range requiredNetworks {
-				if _, exists := networkMap[network]; !exists {
-					flag = false
-
-					break
-				}
-			}
-		}
-
-		if flag {
+	for address, workerNetworksMap := range nodeWorkerNetworksMap {
+		if isValidWorkerAndNetworkNode(workerNetworksMap, workers, networks) {
 			nodes = append(nodes, address)
 		}
 	}
 
-	return nodes, nil
+	return nodes
 }
 
-// matchWorkerAndNetwork matches nodes based on both worker and network criteria.
-// It takes a context, slices of workers and request networks as input parameters.
-// It returns a slice of common node addresses and an error if any occurred.
-func (d *Distributor) matchWorkerAndNetwork(ctx context.Context, workers, requestNetworks []string) ([]common.Address, error) {
-	nodes := make([]common.Address, 0)
-
-	indexers, err := d.databaseClient.FindNodeIndexers(ctx, nil, requestNetworks, workers)
-
-	if err != nil {
-		return nil, err
+// isValidWorkerAndNetworkNode checks if the node matches the required workers and networks.
+func isValidWorkerAndNetworkNode(workerNetworksMap WorkerNetworksMap, workers, requestNetworks []string) bool {
+	if len(workerNetworksMap.Networks) != len(workers) {
+		return false
 	}
 
-	nodeWorkerToNetworksMap := make(map[common.Address]map[string][]string)
+	for worker, networks := range workerNetworksMap.Networks {
+		wid, _ := filter.NameString(worker)
 
-	for _, indexer := range indexers {
-		if _, exists := nodeWorkerToNetworksMap[indexer.Address]; !exists {
-			nodeWorkerToNetworksMap[indexer.Address] = make(map[string][]string)
-		}
+		workerRequiredNetworks := model.WorkerToNetworksMap[wid]
 
-		nodeWorkerToNetworksMap[indexer.Address][indexer.Worker] = append(nodeWorkerToNetworksMap[indexer.Address][indexer.Worker], indexer.Network)
-	}
+		requiredNetworks := findCommonElements(workerRequiredNetworks, requestNetworks)
 
-	for address, workerToNetworksMap := range nodeWorkerToNetworksMap {
-		flag := true
-
-		// check every worker networks
-		for worker, networks := range workerToNetworksMap {
-			wid, _ := filter.NameString(worker)
-
-			workerRequiredNetworks := model.WorkerToNetworksMap[wid]
-
-			requiredNetworks := findCommonStr(workerRequiredNetworks, requestNetworks)
-
-			networkMap := make(map[string]struct{}, len(networks))
-
-			for _, network := range networks {
-				networkMap[network] = struct{}{}
-			}
-
-			// check every network
-			for _, network := range requiredNetworks {
-				if _, exists := networkMap[network]; !exists {
-					flag = false
-
-					break
-				}
-			}
-		}
-
-		if flag {
-			nodes = append(nodes, address)
+		if !AreSliceElementsEqual(networks, requiredNetworks) {
+			return false
 		}
 	}
 
-	return nodes, nil
+	return true
 }
 
-// findCommonStr finds common elements between two string slices.
-// It takes two string slices as input parameters.
-// It returns a slice containing common elements.
-func findCommonStr(slice1, slice2 []string) []string {
+// AreSliceElementsEqual checks if the elements of two string slices are equal.
+func AreSliceElementsEqual(slice1, slice2 []string) bool {
+	if len(slice1) != len(slice2) {
+		return false
+	}
+
+	slice1Set := make(map[string]struct{}, len(slice1))
+
+	for _, str := range slice1 {
+		slice1Set[str] = struct{}{}
+	}
+
+	for _, str := range slice2 {
+		if _, exists := slice1Set[str]; !exists {
+			return false
+		}
+	}
+
+	return true
+}
+
+// findCommonElements finds common elements between two string slices.
+func findCommonElements(slice1, slice2 []string) []string {
 	elementMap := make(map[string]struct{})
 
 	var commonElements []string
