603a78c8f6d7a69cf81eb852c36cfc36f889b33e
Henry <hi@henry.wang>
2024-04-18 21:55:24 +0100

    feat: add /networks endpoint

--- internal/service/hub/handler/nta/network.go
+++ internal/service/hub/handler/nta/network.go
@@ -11,7 +11,29 @@ import (
 	"github.com/rss3-network/protocol-go/schema/filter"
 )
 
-func (n *NTA) GetNetworkWorkers(c echo.Context) error {
+// GetNetworks returns all networks.
+func (n *NTA) GetNetworks(c echo.Context) error {
+	networkList := filter.NetworkValues()
+
+	result := make([]string, 0)
+
+	for _, item := range networkList {
+		networkStr := item.String()
+		// do not add unknown network
+		if networkStr == "unknown" {
+			continue
+		}
+
+		result = append(result, networkStr)
+	}
+
+	return c.JSON(http.StatusOK, nta.Response{
+		Data: result,
+	})
+}
+
+// GetWorkersByNetwork returns workers by Network.
+func (n *NTA) GetWorkersByNetwork(c echo.Context) error {
 	var request nta.NetworkRequest
 
 	if err := c.Bind(&request); err != nil {
