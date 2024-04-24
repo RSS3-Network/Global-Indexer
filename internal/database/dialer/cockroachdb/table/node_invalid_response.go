55eace8ba55a0a0f2a337d5f1cbb52cdc0025b86
Henry <hi@henry.wang>
2024-04-24 13:39:00 +0100

    feat: handle invalid requests

--- internal/database/dialer/cockroachdb/table/node_invalid_response.go
+++ internal/database/dialer/cockroachdb/table/node_invalid_response.go
@@ -2,9 +2,6 @@ package table
 
 import (
 	"encoding/json"
-	"fmt"
-	"net/url"
-	"strings"
 	"time"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -12,17 +9,16 @@ import (
 )
 
 type NodeInvalidResponse struct {
-	ID                uint64                           `gorm:"id;primaryKey"`
-	EpochID           uint64                           `gorm:"column:epoch_id"`
-	Status            schema.NodeInvalidResponseStatus `gorm:"column:status"`
-	ValidatorNode     common.Address                   `gorm:"column:validator_node"`
-	ValidatorRequest  string                           `gorm:"column:validator_request"`
-	ValidatorResponse json.RawMessage                  `gorm:"column:validator_response"`
-	VerifiedNode      common.Address                   `gorm:"column:verified_node"`
-	VerifiedRequest   string                           `gorm:"column:verified_request"`
-	VerifiedResponse  json.RawMessage                  `gorm:"column:verified_response"`
-	CreatedAt         time.Time                        `gorm:"column:created_at"`
-	UpdatedAt         time.Time                        `gorm:"column:updated_at"`
+	ID                uint64                         `gorm:"id;primaryKey"`
+	EpochID           uint64                         `gorm:"column:epoch_id"`
+	InvalidType       schema.NodeInvalidResponseType `gorm:"column:invalid_type"`
+	Request           string                         `gorm:"column:request"`
+	ValidatorNodes    []common.Address               `gorm:"column:validator_nodes"`
+	ValidatorResponse json.RawMessage                `gorm:"column:validator_response"`
+	FaultyNode        common.Address                 `gorm:"column:faulty_node"`
+	FaultyResponse    json.RawMessage                `gorm:"column:faulty_response"`
+	CreatedAt         time.Time                      `gorm:"column:created_at"`
+	UpdatedAt         time.Time                      `gorm:"column:updated_at"`
 }
 
 func (*NodeInvalidResponse) TableName() string {
@@ -31,46 +27,24 @@ func (*NodeInvalidResponse) TableName() string {
 
 func (n *NodeInvalidResponse) Import(nodeResponseFailure *schema.NodeInvalidResponse) {
 	n.EpochID = nodeResponseFailure.EpochID
-	n.Status = nodeResponseFailure.Status
-	n.ValidatorNode = nodeResponseFailure.ValidatorNode
-	n.ValidatorRequest = nodeResponseFailure.ValidatorRequest
+	n.InvalidType = nodeResponseFailure.InvalidType
+	n.Request = nodeResponseFailure.Request
+	n.ValidatorNodes = nodeResponseFailure.ValidatorNodes
 	n.ValidatorResponse = nodeResponseFailure.ValidatorResponse
-	n.VerifiedNode = nodeResponseFailure.VerifiedNode
-	n.VerifiedRequest = nodeResponseFailure.VerifiedRequest
-	n.VerifiedResponse = nodeResponseFailure.VerifiedResponse
+	n.FaultyNode = nodeResponseFailure.FaultyNode
+	n.FaultyResponse = nodeResponseFailure.FaultyResponse
 }
 
-func (n *NodeInvalidResponse) Export() (*schema.NodeInvalidResponse, error) {
-	validatorRequest, err := extractPathAndParams(n.ValidatorRequest)
-	if err != nil {
-		return nil, err
-	}
-
-	verifiedRequest, err := extractPathAndParams(n.VerifiedRequest)
-	if err != nil {
-		return nil, err
-	}
-
+func (n *NodeInvalidResponse) Export() *schema.NodeInvalidResponse {
 	return &schema.NodeInvalidResponse{
 		ID:                n.ID,
 		EpochID:           n.EpochID,
-		Status:            n.Status,
-		ValidatorNode:     n.ValidatorNode,
-		ValidatorRequest:  validatorRequest,
+		InvalidType:       n.InvalidType,
+		Request:           n.Request,
+		ValidatorNodes:    n.ValidatorNodes,
 		ValidatorResponse: n.ValidatorResponse,
-		VerifiedNode:      n.VerifiedNode,
-		VerifiedRequest:   verifiedRequest,
-		VerifiedResponse:  n.VerifiedResponse,
+		FaultyNode:        n.FaultyNode,
+		FaultyResponse:    n.FaultyResponse,
 		CreatedAt:         n.CreatedAt.Unix(),
-	}, nil
-}
-
-func extractPathAndParams(endpoint string) (string, error) {
-	parsedURL, err := url.Parse(endpoint)
-	if err != nil {
-		fmt.Println("Error parsing URL:", err)
-		return "", err
 	}
-
-	return strings.TrimPrefix(endpoint, parsedURL.Scheme+"://"+parsedURL.Host), nil
 }
--- internal/service/hub/handler/dsl/distributor/process_response.go
+++ internal/service/hub/handler/dsl/distributor/process_response.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
+	"net/url"
+	"strings"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/rss3-network/global-indexer/internal/database"
@@ -47,6 +50,10 @@ func (d *Distributor) processActivitiesResponses(responses []*model.DataResponse
 
 	epochID := d.processNodeInvalidResponse(context.Background(), responses)
 
+	if epochID == 0 {
+		return
+	}
+
 	zap.L().Info("complete activity responses verify", zap.Any("responses", len(responses)))
 
 	d.simpleEnforcer.VerifyPartialResponses(ctx, epochID, responses)
@@ -54,15 +61,24 @@ func (d *Distributor) processActivitiesResponses(responses []*model.DataResponse
 
 // processNodeInvalidResponse finds the valid response data and saves the invalid responses.
 func (d *Distributor) processNodeInvalidResponse(ctx context.Context, responses []*model.DataResponse) uint64 {
+	validatorNodes, request, validatorResponse, err := getValidResponseData(responses)
+	if err != nil {
+		zap.L().Error("get valid response data", zap.Error(err))
+		return 0
+	}
+
+	// If all responses are valid, return 0.
+	if len(validatorNodes) == len(responses) {
+		return 0
+	}
+
 	epochID, err := d.getLatestEpochID(ctx)
 	if err != nil {
 		zap.L().Error("get latest epoch event from database", zap.Error(err))
 		return 0
 	}
 
-	validatorNode, validatorRequest, validatorResponse := getValidResponseData(responses)
-
-	d.saveInvalidResponses(ctx, epochID, validatorNode, validatorRequest, validatorResponse, responses)
+	d.saveInvalidResponses(ctx, epochID, validatorNodes, request, validatorResponse, responses)
 
 	return epochID
 }
@@ -82,30 +98,52 @@ func (d *Distributor) getLatestEpochID(ctx context.Context) (uint64, error) {
 }
 
 // getValidResponseData returns the valid response data which valid points are greater than 0.
-func getValidResponseData(responses []*model.DataResponse) (common.Address, string, json.RawMessage) {
+func getValidResponseData(responses []*model.DataResponse) ([]common.Address, string, json.RawMessage, error) {
+	var (
+		validatorNodes []common.Address
+		data           json.RawMessage
+	)
+
 	for _, response := range responses {
 		if response.ValidPoint > 0 {
-			return response.Address, response.Endpoint, response.Data
+			validatorNodes = append(validatorNodes, response.Address)
+			data = response.Data
 		}
 	}
 
-	return common.Address{}, "", nil
+	request, err := extractPathAndParams(responses[0].Endpoint)
+	if err != nil {
+		return nil, "", nil, err
+	}
+
+	return validatorNodes, request, data, nil
+}
+
+// extractPathAndParams extracts the path and params from the endpoint.
+func extractPathAndParams(endpoint string) (string, error) {
+	parsedURL, err := url.Parse(endpoint)
+	if err != nil {
+		fmt.Println("Error parsing URL:", err)
+		return "", err
+	}
+
+	return strings.TrimPrefix(endpoint, parsedURL.Scheme+"://"+parsedURL.Host), nil
 }
 
 // saveInvalidResponses saves the responses which invalid points are greater than 0 and status is challengeable.
-func (d *Distributor) saveInvalidResponses(ctx context.Context, epochID uint64, validatorNode common.Address, validatorRequest string, validatorResponse json.RawMessage, responses []*model.DataResponse) {
+func (d *Distributor) saveInvalidResponses(ctx context.Context, epochID uint64, validatorNodes []common.Address, request string, validatorResponse json.RawMessage, responses []*model.DataResponse) {
 	for _, response := range responses {
 		if response.InvalidPoint > 0 {
 			err := d.databaseClient.SaveNodeInvalidResponse(ctx, &schema.NodeInvalidResponse{
 				EpochID:           epochID,
-				Status:            schema.NodeInvalidResponseStatusChallengeable,
-				ValidatorNode:     validatorNode,
-				ValidatorRequest:  validatorRequest,
+				InvalidType:       lo.Ternary(response.Err != nil, schema.NodeInvalidResponseTypeError, schema.NodeInvalidResponseTypeError),
+				ValidatorNodes:    validatorNodes,
+				Request:           request,
 				ValidatorResponse: validatorResponse,
-				VerifiedNode:      response.Address,
-				VerifiedRequest:   response.Endpoint,
-				VerifiedResponse:  lo.Ternary(response.Err != nil, json.RawMessage(response.Err.Error()), response.Data),
+				FaultyNode:        response.Address,
+				FaultyResponse:    lo.Ternary(response.Err != nil, json.RawMessage(response.Err.Error()), response.Data),
 			})
+
 			if err != nil {
 				zap.L().Error("save node invalid response", zap.Error(err))
 			}
