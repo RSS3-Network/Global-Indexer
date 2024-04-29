c486ceece74a2dd6f01858b76584c4ea1e7cc3db
Nya Candy <dev@candinya.com>
2024-04-29 17:53:25 +0800

    fix: lint

--- internal/database/dialer/cockroachdb/table/node.go
+++ internal/database/dialer/cockroachdb/table/node.go
@@ -27,7 +27,7 @@ type Node struct {
 	MinTokensToStake       decimal.Decimal   `gorm:"column:min_tokens_to_stake;type:decimal;"`
 	APY                    decimal.Decimal   `gorm:"column:apy;type:decimal;default:0;"`
 	Score                  decimal.Decimal   `gorm:"column:score;type:decimal;default:0;index:idx_score,sort:desc;"`
-	Type             string          `gorm:"column:type;type:text;"`
+	Type                   string            `gorm:"column:type;type:text;"`
 	CreatedAt              time.Time         `gorm:"column:created_at;type:timestamp with time zone;autoCreateTime;not null;default:now();index:idx_is_public,priority:2,sort:desc;index:idx_created_at,sort:desc;"`
 	UpdatedAt              time.Time         `gorm:"column:updated_at;type:timestamp with time zone;autoUpdateTime;not null;default:now();"`
 }
