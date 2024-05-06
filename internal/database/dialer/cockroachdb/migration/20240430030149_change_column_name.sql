1e620bfa9761c1c12427a91d6d03076172076c76
Nya Candy <dev@candinya.com>
2024-05-06 12:11:25 +0800

    chore: add missing migrations
    for #208 epoch_item -> node_reward_record

--- internal/database/dialer/cockroachdb/migration/20240430030149_change_column_name.sql
+++ internal/database/dialer/cockroachdb/migration/20240430030149_change_column_name.sql
@@ -8,10 +8,14 @@ ALTER TABLE "epoch_item"
     RENAME COLUMN "tax_amounts" TO "tax_collected";
 ALTER TABLE "epoch_item"
     RENAME COLUMN "request_counts" TO "request_count";
+ALTER TABLE "epoch_item"
+    RENAME TO "node_reward_record";
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
+ALTER TABLE "node_reward_record"
+    RENAME TO "epoch_item";
 ALTER TABLE "epoch_item"
     RENAME COLUMN "request_count" TO "request_counts";
 ALTER TABLE "epoch_item"
