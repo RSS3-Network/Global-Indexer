9ab0dcee68afa45caa7a6209f5f203571f9ce0e3
brucexc <108378464+brucexc@users.noreply.github.com>
2024-05-14 18:42:07 -0400

    feat(hub)!: /networks API and update OpenAPI (#216)
    Co-authored-by: Henry <hi@henry.wang>

--- internal/service/hub/handler/nta/network.go
+++ internal/service/hub/handler/nta/network.go
@@ -2,55 +2,104 @@ package nta
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/rss3-network/global-indexer/internal/service/hub/handler/dsl/model"
 	"github.com/rss3-network/global-indexer/internal/service/hub/model/errorx"
 	"github.com/rss3-network/global-indexer/internal/service/hub/model/nta"
-	"github.com/rss3-network/protocol-go/schema/filter"
+	"github.com/rss3-network/global-indexer/schema"
+	"github.com/samber/lo"
 )
 
 // GetNetworks returns all networks.
 func (n *NTA) GetNetworks(c echo.Context) error {
-	networkList := filter.NetworkValues()
-
-	result := make([]string, 0)
-
-	for _, item := range networkList {
-		networkStr := item.String()
-		// do not add unknown network
-		if networkStr == "unknown" {
-			continue
-		}
-
-		result = append(result, networkStr)
+	endpoint, err := n.getNodeEndpoint(c)
+	if err != nil {
+		return errorx.BadParamsError(c, fmt.Errorf("get node endpoint: %w", err))
 	}
 
-	return c.JSON(http.StatusOK, nta.Response{
-		Data: result,
-	})
+	return n.fetchResponse(c, fmt.Sprintf("%s/networks", endpoint))
 }
 
 // GetWorkersByNetwork returns workers by Network.
 func (n *NTA) GetWorkersByNetwork(c echo.Context) error {
 	var request nta.NetworkRequest
 
-	if err := c.Bind(&request); err != nil {
+	if err := n.bindAndValidateRequest(c, &request); err != nil {
+		return err
+	}
+
+	endpoint, err := n.getNodeEndpoint(c)
+	if err != nil {
+		return errorx.BadParamsError(c, fmt.Errorf("get node endpoint: %w", err))
+	}
+
+	return n.fetchResponse(c, fmt.Sprintf("%s/networks/%s/list-workers", endpoint, request.Network))
+}
+
+// GetWorkerDetail returns worker with detail configuration.
+func (n *NTA) GetWorkerDetail(c echo.Context) error {
+	var request nta.WorkerRequest
+
+	if err := n.bindAndValidateRequest(c, &request); err != nil {
+		return err
+	}
+
+	endpoint, err := n.getNodeEndpoint(c)
+
+	if err != nil {
+		return errorx.BadParamsError(c, fmt.Errorf("get node endpoint: %w", err))
+	}
+
+	return n.fetchResponse(c, fmt.Sprintf("%s/networks/%s/workers/%s", endpoint, request.Network, request.Worker))
+}
+
+// bindAndValidateRequest binds and validates the request.
+func (n *NTA) bindAndValidateRequest(c echo.Context, request interface{}) error {
+	if err := c.Bind(request); err != nil {
 		return errorx.BadParamsError(c, fmt.Errorf("bind request: %w", err))
 	}
 
-	if err := c.Validate(&request); err != nil {
+	if err := c.Validate(request); err != nil {
 		return errorx.ValidateFailedError(c, fmt.Errorf("validate failed: %w", err))
 	}
 
-	nid, err := filter.NetworkString(request.Network)
+	return nil
+}
 
+// getNodeEndpoint returns the endpoint of the node with the highest points.
+func (n *NTA) getNodeEndpoint(c echo.Context) (string, error) {
+	stats, err := n.databaseClient.FindNodeStats(c.Request().Context(), &schema.StatQuery{
+		ValidRequest: lo.ToPtr(model.DemotionCountBeforeSlashing),
+		Limit:        lo.ToPtr(model.RequiredQualifiedNodeCount),
+		PointsOrder:  lo.ToPtr("DESC"),
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	if len(stats) == 0 {
+		return "", fmt.Errorf("no available nodes")
+	}
+
+	return stats[0].Endpoint, nil
+}
+
+// fetchResponse fetches data from the provided endpoint.
+func (n *NTA) fetchResponse(c echo.Context, url string) error {
+	response, err := n.httpClient.Fetch(c.Request().Context(), url)
 	if err != nil {
 		return errorx.BadParamsError(c, fmt.Errorf("network: %w", err))
 	}
 
-	return c.JSON(http.StatusOK, nta.Response{
-		Data: model.NetworkToWorkersMap[nid],
-	})
+	data, err := io.ReadAll(response)
+
+	if err != nil {
+		return errorx.BadParamsError(c, fmt.Errorf("read response: %w", err))
+	}
+
+	return c.JSONBlob(http.StatusOK, data)
 }
