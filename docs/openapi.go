498fdd005a54c028d4eb0477859c48efb7c58226
polebug <polebugfly@gmail.com>
2024-06-19 18:53:40 +0800

    feat: generate metadata schemas

--- docs/openapi.go
+++ docs/openapi.go
@@ -3,16 +3,23 @@ package docs
 import (
 	"embed"
 	"fmt"
+	"math/big"
 	"os"
+	"reflect"
+	"runtime"
 	"sort"
+	"strings"
 
 	"github.com/rss3-network/global-indexer/internal/service/hub/model/errorx"
 	"github.com/rss3-network/node/schema/worker"
 	"github.com/rss3-network/protocol-go/schema"
 	"github.com/rss3-network/protocol-go/schema/activity"
+	"github.com/rss3-network/protocol-go/schema/metadata"
 	"github.com/rss3-network/protocol-go/schema/network"
 	"github.com/rss3-network/protocol-go/schema/tag"
+	"github.com/rss3-network/protocol-go/schema/typex"
 	"github.com/samber/lo"
+	"github.com/shopspring/decimal"
 	"github.com/tidwall/sjson"
 	"go.uber.org/zap"
 )
@@ -32,6 +39,11 @@ func Generate() ([]byte, error) {
 
 	// Generate network, tag, platform, direction enum.
 	if file, err = generateEnum(file); err != nil {
+		return nil, err
+	}
+
+	// Generate metadata action schema.
+	if file, err = generateMetadataAction(file); err != nil {
 		return nil, err
 	}
 
@@ -112,4 +124,163 @@ func generateEnum(file []byte) ([]byte, error) {
 	}
 
 	return file, nil
+}
+
+func generateMetadataAction(file []byte) ([]byte, error) {
+	// Generate all TransactionMetadataActionSchemas
+	schemas := generateTransactionMetadataActionSchemas()
+
+	// Add the generated schemas to the components.schemas section of the OpenAPI document
+	for key, schema := range schemas {
+		var err error
+		file, err = sjson.SetBytes(file, fmt.Sprintf("components.schemas.%s", key), schema)
+
+		if err != nil {
+			return nil, fmt.Errorf("sjson set schema err: %w", err)
+		}
+	}
+
+	// Prepare anyOf array for components.schemas.Action.properties.metadata
+	anyOfArray := make([]map[string]interface{}, 0)
+
+	for key := range schemas {
+		anyOfArray = append(anyOfArray, map[string]interface{}{
+			"$ref": fmt.Sprintf("#/components/schemas/%s", key),
+		})
+	}
+
+	// Add the anyOf array to components.schemas.Action.properties.metadata
+	file, err := sjson.SetBytes(file, "components.schemas.Action.properties.metadata.anyOf", anyOfArray)
+	if err != nil {
+		return nil, fmt.Errorf("sjson set anyOf err: %w", err)
+	}
+
+	return file, nil
+}
+
+func generateTransactionMetadataActionSchemas() map[string]interface{} {
+	schemas := make(map[string]interface{})
+
+	schemas[typex.TransactionApproval.String()] = generateMetadataObject(reflect.TypeOf(metadata.TransactionApproval{}))
+	schemas[typex.TransactionBridge.String()] = generateMetadataObject(reflect.TypeOf(metadata.TransactionBridge{}))
+
+	return schemas
+}
+
+func generateMetadataObject(t reflect.Type) map[string]interface{} {
+	object := map[string]interface{}{
+		"type":       "object",
+		"properties": map[string]interface{}{},
+	}
+
+	properties := object["properties"].(map[string]interface{})
+
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		// Remove 'omitempty' from the JSON tag to get the actual field name
+		fieldName := strings.Split(field.Tag.Get("json"), ",")[0]
+		if fieldName == "" {
+			fieldName = field.Name
+		}
+
+		// Check if the field type has a corresponding Strings function
+		if method, ok := hasEnumStringsFunction(field.Type); ok {
+			fieldSchema := map[string]interface{}{
+				"type": "string",
+				"enum": getEnumStrings(method),
+			}
+			properties[fieldName] = fieldSchema
+		} else {
+			// Handle pointer types first
+			if field.Type.Kind() == reflect.Ptr {
+				elemType := field.Type.Elem()
+
+				if elemType == reflect.TypeOf(decimal.Decimal{}) || elemType == reflect.TypeOf(big.Int{}) {
+					properties[fieldName] = map[string]interface{}{
+						"type": "string",
+					}
+
+					continue
+				}
+
+				if elemType.Kind() == reflect.Struct {
+					properties[fieldName] = generateMetadataObject(elemType)
+
+					continue
+				}
+			}
+
+			// Then handle struct types
+			if field.Type.Kind() == reflect.Struct {
+				fieldSchema := generateMetadataObject(field.Type)
+				properties[fieldName] = fieldSchema
+			} else {
+				fieldSchema := map[string]interface{}{
+					"type": transformOpenAPIType(field.Type),
+				}
+				properties[fieldName] = fieldSchema
+			}
+		}
+	}
+
+	return object
+}
+
+func transformOpenAPIType(t reflect.Type) string {
+	switch t.Kind() {
+	case reflect.String:
+		return "string"
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return "integer"
+	case reflect.Float32, reflect.Float64:
+		return "number"
+	case reflect.Bool:
+		return "boolean"
+	case reflect.Slice, reflect.Array:
+		return "array"
+	case reflect.Ptr:
+		elemType := t.Elem()
+
+		if elemType == reflect.TypeOf(decimal.Decimal{}) || elemType == reflect.TypeOf(big.Int{}) {
+			return "string"
+		}
+
+		return transformOpenAPIType(elemType)
+	case reflect.Struct:
+		if t == reflect.TypeOf(decimal.Decimal{}) || t == reflect.TypeOf(big.Int{}) {
+			return "string"
+		}
+
+		return "object"
+	default:
+		return "string"
+	}
+}
+
+func hasEnumStringsFunction(t reflect.Type) (reflect.Value, bool) {
+	if t.Name() == "" {
+		return reflect.Value{}, false
+	}
+
+	funcName := t.Name() + "Strings"
+	globalFuncs := []interface{}{
+		metadata.TransactionApprovalActionStrings,
+		network.NetworkStrings,
+		// TODO: Add more global functions here
+	}
+
+	for _, f := range globalFuncs {
+		funcFullName := runtime.FuncForPC(reflect.ValueOf(f).Pointer()).Name()
+		if strings.HasSuffix(funcFullName, funcName) {
+			return reflect.ValueOf(f), true
+		}
+	}
+
+	return reflect.Value{}, false
+}
+
+func getEnumStrings(method reflect.Value) []string {
+	results := method.Call(nil)
+	return results[0].Interface().([]string)
 }
