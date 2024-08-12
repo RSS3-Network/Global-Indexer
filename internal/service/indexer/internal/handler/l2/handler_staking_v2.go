ee5143c4908d35ecf1a1b6bcb2726546868d3653
polebug <polebugfly@gmail.com>
2024-08-12 17:30:56 +0800

    feat(indexer): upgrade staking events (#336)

--- internal/service/indexer/internal/handler/l2/handler_staking_v2.go
+++ internal/service/indexer/internal/handler/l2/handler_staking_v2.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/rss3-network/global-indexer/common/ethereum"
 	"github.com/rss3-network/global-indexer/contract/l2"
@@ -18,14 +19,21 @@ import (
 	"github.com/sourcegraph/conc/pool"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
+	"go.uber.org/zap"
 )
 
 func (h *handler) indexStakingV2Log(ctx context.Context, header *types.Header, transaction *types.Transaction, receipt *types.Receipt, log *types.Log, databaseTransaction database.Client) error {
+	if eventHash := log.Topics[0]; eventHash == common.HexToHash("0x8adb7a84b2998a8d11cd9284395f95d5a99f160be785ae79998c654979bd3d9a") {
+		zap.L().Info("staking v2 log", zap.String("event", "Staked"), zap.String("hash", eventHash.Hex()))
+	}
+
 	switch eventHash := log.Topics[0]; eventHash {
 	case l2.EventHashStakingV1Staked: // The event hash is the same as the staking v1 contract.
 		return h.indexStakingV2StakedLog(ctx, header, transaction, receipt, log, databaseTransaction)
 	case l2.EventHashStakingV2ChipsMerged:
 		return h.indexStakingV2ChipsMergedLog(ctx, header, transaction, receipt, log, databaseTransaction)
+	case l2.EventHashStakingV2WithdrawalClaimed:
+		return h.indexStakingV2WithdrawalClaimedLog(ctx, header, transaction, receipt, log, databaseTransaction)
 	default:
 		return h.indexStakingV1Log(ctx, header, transaction, receipt, log, databaseTransaction)
 	}
@@ -170,6 +178,41 @@ func (h *handler) indexStakingV2ChipsMergedLog(ctx context.Context, header *type
 
 	if _, err := h.contractStakingV2.ParseChipsMerged(*log); err != nil {
 		return fmt.Errorf("parse ChipsMerged event: %w", err)
+	}
+
+	return nil
+}
+
+func (h *handler) indexStakingV2WithdrawalClaimedLog(ctx context.Context, header *types.Header, transaction *types.Transaction, receipt *types.Receipt, log *types.Log, databaseTransaction database.Client) error {
+	ctx, span := otel.Tracer("").Start(ctx, "indexStakingV2WithdrawalClaimedLog")
+	defer span.End()
+
+	span.SetAttributes(
+		attribute.Int64("block.number", header.Number.Int64()),
+		attribute.Stringer("block.hash", header.Hash()),
+		attribute.Stringer("transaction.hash", transaction.Hash()),
+		attribute.Int("log.index", int(log.Index)),
+	)
+
+	event, err := h.contractStakingV2.ParseWithdrawalClaimed(*log)
+	if err != nil {
+		return fmt.Errorf("parse WithdrawalClaimed event: %w", err)
+	}
+
+	stakeEvent := schema.StakeEvent{
+		ID:                common.BigToHash(event.RequestId),
+		Type:              schema.StakeEventTypeWithdrawClaimed,
+		TransactionHash:   transaction.Hash(),
+		TransactionIndex:  receipt.TransactionIndex,
+		TransactionStatus: receipt.Status,
+		BlockHash:         header.Hash(),
+		BlockNumber:       header.Number,
+		BlockTimestamp:    time.Unix(int64(header.Time), 0),
+		Finalized:         h.finalized,
+	}
+
+	if err := databaseTransaction.SaveStakeEvent(ctx, &stakeEvent); err != nil {
+		return fmt.Errorf("save stake event: %w", err)
 	}
 
 	return nil
