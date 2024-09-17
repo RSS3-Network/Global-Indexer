f67cbe30d379dafb2381894e8d4d4f0db4441652
brucexc <brucexc@rss3.io>
2024-09-16 23:49:05 -0400

    chore: handle status test

--- internal/service/hub/handler/dsl/enforcer/handle_status_test.go
+++ internal/service/hub/handler/dsl/enforcer/handle_status_test.go
@@ -22,310 +22,156 @@ var (
 	workerStatusNodeOnline    = `{"data":{"decentralized":[{"network":"farcaster","worker":"core","tags":null,"platform":"Unknown","status":"Ready","remote_state":0,"indexed_state":0}],"rss":null,"federated":null}}`
 )
 
-func Test_NodeInfoUnavailableReturnsOffline(t *testing.T) {
+func TestDetermineStatus(t *testing.T) {
 	t.Parallel()
 
-	ctx := context.Background()
-
-	mockClient := new(MockHTTPClient)
-	mockClient.On("FetchWithMethod", mock.Anything, "http://localhost:8080/workers_status").Return(io.NopCloser(bytes.NewReader([]byte(workerStatusNodeIndexing))), nil)
-	mockClient.On("FetchWithMethod", mock.Anything, "http://localhost:8080/info").Return(io.NopCloser(bytes.NewReader([]byte(``))), errors.New("info"))
-
-	enforcer := &SimpleEnforcer{
-		httpClient: mockClient,
+	tests := []struct {
+		name            string
+		workerStatus    string
+		nodeInfo        string
+		initialStatus   schema.NodeStatus
+		minVersion      string
+		expectedStatus  schema.NodeStatus
+		expectedErrPath string
+	}{
+		{
+			name:            "NodeInfoUnavailable",
+			workerStatus:    workerStatusNodeIndexing,
+			nodeInfo:        "",
+			initialStatus:   schema.NodeStatusInitializing,
+			minVersion:      "1.0.0",
+			expectedStatus:  schema.NodeStatusOffline,
+			expectedErrPath: "info",
+		},
+		{
+			name:            "WorkerStatusUnavailable",
+			workerStatus:    "",
+			nodeInfo:        nodeInfo,
+			initialStatus:   schema.NodeStatusInitializing,
+			minVersion:      "1.0.0",
+			expectedStatus:  schema.NodeStatusOffline,
+			expectedErrPath: "workers_status",
+		},
+		{
+			name:            "RegisteredToOutdated",
+			workerStatus:    workerStatusNodeIndexing,
+			nodeInfo:        nodeInfo,
+			initialStatus:   schema.NodeStatusRegistered,
+			minVersion:      "1.1.0",
+			expectedStatus:  schema.NodeStatusOutdated,
+			expectedErrPath: "",
+		},
+		{
+			name:            "RegisteredStaysRegistered",
+			workerStatus:    workerStatusNodeUnhealthy,
+			nodeInfo:        nodeInfo,
+			initialStatus:   schema.NodeStatusRegistered,
+			minVersion:      "1.0.0",
+			expectedStatus:  schema.NodeStatusRegistered,
+			expectedErrPath: "",
+		},
+		{
+			name:            "RegisteredToInitializing",
+			workerStatus:    workerStatusNodeIndexing,
+			nodeInfo:        nodeInfo,
+			initialStatus:   schema.NodeStatusRegistered,
+			minVersion:      "1.0.0",
+			expectedStatus:  schema.NodeStatusInitializing,
+			expectedErrPath: "",
+		},
+		{
+			name:            "OutdatedStaysOutdated",
+			workerStatus:    workerStatusNodeIndexing,
+			nodeInfo:        nodeInfo,
+			initialStatus:   schema.NodeStatusOutdated,
+			minVersion:      "1.1.0",
+			expectedStatus:  schema.NodeStatusOutdated,
+			expectedErrPath: "",
+		},
+		{
+			name:            "OutdatedToRegistered",
+			workerStatus:    workerStatusNodeUnhealthy,
+			nodeInfo:        nodeInfo,
+			initialStatus:   schema.NodeStatusOutdated,
+			minVersion:      "1.0.0",
+			expectedStatus:  schema.NodeStatusRegistered,
+			expectedErrPath: "",
+		},
+		{
+			name:            "OutdatedToInitializing",
+			workerStatus:    workerStatusNodeIndexing,
+			nodeInfo:        nodeInfo,
+			initialStatus:   schema.NodeStatusOutdated,
+			minVersion:      "1.0.0",
+			expectedStatus:  schema.NodeStatusInitializing,
+			expectedErrPath: "",
+		},
+		{
+			name:            "InitializingToOutdated",
+			workerStatus:    workerStatusNodeIndexing,
+			nodeInfo:        nodeInfo,
+			initialStatus:   schema.NodeStatusInitializing,
+			minVersion:      "1.1.0",
+			expectedStatus:  schema.NodeStatusOutdated,
+			expectedErrPath: "",
+		},
+		{
+			name:            "InitializingToRegistered",
+			workerStatus:    workerStatusNodeUnhealthy,
+			nodeInfo:        nodeInfo,
+			initialStatus:   schema.NodeStatusInitializing,
+			minVersion:      "1.0.0",
+			expectedStatus:  schema.NodeStatusRegistered,
+			expectedErrPath: "",
+		},
+		{
+			name:            "InitializingStaysInitializing",
+			workerStatus:    workerStatusNodeIndexing,
+			nodeInfo:        nodeInfo,
+			initialStatus:   schema.NodeStatusInitializing,
+			minVersion:      "1.0.0",
+			expectedStatus:  schema.NodeStatusInitializing,
+			expectedErrPath: "",
+		},
 	}
-	stat := &schema.Stat{
-		Address:     common.Address{0},
-		Endpoint:    "http://localhost:8080",
-		AccessToken: "token",
-		Epoch:       1,
-		Status:      schema.NodeStatusInitializing,
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := context.Background()
+			mockClient := setupMockClient(tt.workerStatus, tt.nodeInfo)
+			enforcer := &SimpleEnforcer{httpClient: mockClient}
+			stat := &schema.Stat{
+				Address:     common.Address{},
+				Endpoint:    "http://localhost:8080",
+				AccessToken: "token",
+				Epoch:       1,
+				Status:      tt.initialStatus,
+			}
+
+			minVersion, _ := version.NewVersion(tt.minVersion)
+			status, errPath := enforcer.determineStatus(ctx, stat, minVersion)
+
+			assert.Equal(t, tt.expectedStatus, status)
+			assert.Equal(t, tt.expectedErrPath, errPath)
+		})
 	}
-
-	minVersion, _ := version.NewVersion("1.0.0")
-
-	status, errPath := enforcer.determineStatus(ctx, stat, minVersion)
-
-	assert.Equal(t, schema.NodeStatusOffline, status)
-	assert.Equal(t, "info", errPath)
 }
 
-func Test_NodeWorkerStatusUnavailableReturnsOffline(t *testing.T) {
-	t.Parallel()
-
-	ctx := context.Background()
-
+func setupMockClient(workerStatus, nodeInfo string) *MockHTTPClient {
 	mockClient := new(MockHTTPClient)
-	mockClient.On("FetchWithMethod", mock.Anything, "http://localhost:8080/workers_status").Return(io.NopCloser(bytes.NewReader([]byte(``))), errors.New("workers_status"))
-	mockClient.On("FetchWithMethod", mock.Anything, "http://localhost:8080/info").Return(io.NopCloser(bytes.NewReader([]byte(nodeInfo))), nil)
-
-	enforcer := &SimpleEnforcer{
-		httpClient: mockClient,
+	if workerStatus == "" {
+		mockClient.On("FetchWithMethod", mock.Anything, "http://localhost:8080/workers_status").
+			Return(io.NopCloser(bytes.NewReader([]byte(""))), errors.New("workers_status"))
+	} else {
+		mockClient.On("FetchWithMethod", mock.Anything, "http://localhost:8080/workers_status").
+			Return(io.NopCloser(bytes.NewReader([]byte(workerStatus))), nil)
 	}
-	stat := &schema.Stat{
-		Address:     common.Address{0},
-		Endpoint:    "http://localhost:8080",
-		AccessToken: "token",
-		Epoch:       1,
-		Status:      schema.NodeStatusInitializing,
+	if nodeInfo == "" {
+		mockClient.On("FetchWithMethod", mock.Anything, "http://localhost:8080/info").
+			Return(io.NopCloser(bytes.NewReader([]byte(""))), errors.New("info"))
+	} else {
+		mockClient.On("FetchWithMethod", mock.Anything, "http://localhost:8080/info").
+			Return(io.NopCloser(bytes.NewReader([]byte(nodeInfo))), nil)
 	}
-
-	minVersion, _ := version.NewVersion("1.0.0")
-
-	status, errPath := enforcer.determineStatus(ctx, stat, minVersion)
-
-	assert.Equal(t, schema.NodeStatusOffline, status)
-	assert.Equal(t, "workers_status", errPath)
-}
-
-func Test_NodeStatusRegisteredReturnsOutdated(t *testing.T) {
-	t.Parallel()
-
-	ctx := context.Background()
-
-	mockClient := new(MockHTTPClient)
-	mockClient.On("FetchWithMethod", mock.Anything, "http://localhost:8080/workers_status").Return(io.NopCloser(bytes.NewReader([]byte(workerStatusNodeIndexing))), nil)
-	mockClient.On("FetchWithMethod", mock.Anything, "http://localhost:8080/info").Return(io.NopCloser(bytes.NewReader([]byte(nodeInfo))), nil)
-
-	enforcer := &SimpleEnforcer{
-		httpClient: mockClient,
-	}
-	stat := &schema.Stat{
-		Address:     common.Address{0},
-		Endpoint:    "http://localhost:8080",
-		AccessToken: "token",
-		Epoch:       1,
-		Status:      schema.NodeStatusRegistered,
-	}
-
-	minVersion, _ := version.NewVersion("1.1.0")
-
-	status, errPath := enforcer.determineStatus(ctx, stat, minVersion)
-
-	assert.Equal(t, schema.NodeStatusOutdated, status)
-	assert.Equal(t, "", errPath)
-}
-
-func Test_NodeStatusRegisteredReturnsRegistered(t *testing.T) {
-	t.Parallel()
-
-	ctx := context.Background()
-
-	mockClient := new(MockHTTPClient)
-	mockClient.On("FetchWithMethod", mock.Anything, "http://localhost:8080/workers_status").Return(io.NopCloser(bytes.NewReader([]byte(workerStatusNodeUnhealthy))), nil)
-	mockClient.On("FetchWithMethod", mock.Anything, "http://localhost:8080/info").Return(io.NopCloser(bytes.NewReader([]byte(nodeInfo))), nil)
-
-	enforcer := &SimpleEnforcer{
-		httpClient: mockClient,
-	}
-	stat := &schema.Stat{
-		Address:     common.Address{0},
-		Endpoint:    "http://localhost:8080",
-		AccessToken: "token",
-		Epoch:       1,
-		Status:      schema.NodeStatusRegistered,
-	}
-
-	minVersion, _ := version.NewVersion("1.0.0")
-
-	status, errPath := enforcer.determineStatus(ctx, stat, minVersion)
-
-	assert.Equal(t, schema.NodeStatusRegistered, status)
-	assert.Equal(t, "", errPath)
-}
-
-func Test_NodeStatusRegisteredReturnsInitializing(t *testing.T) {
-	t.Parallel()
-
-	ctx := context.Background()
-
-	mockClient := new(MockHTTPClient)
-	mockClient.On("FetchWithMethod", mock.Anything, "http://localhost:8080/workers_status").Return(io.NopCloser(bytes.NewReader([]byte(workerStatusNodeIndexing))), nil)
-	mockClient.On("FetchWithMethod", mock.Anything, "http://localhost:8080/info").Return(io.NopCloser(bytes.NewReader([]byte(nodeInfo))), nil)
-
-	enforcer := &SimpleEnforcer{
-		httpClient: mockClient,
-	}
-	stat := &schema.Stat{
-		Address:     common.Address{0},
-		Endpoint:    "http://localhost:8080",
-		AccessToken: "token",
-		Epoch:       1,
-		Status:      schema.NodeStatusRegistered,
-	}
-
-	minVersion, _ := version.NewVersion("1.0.0")
-
-	status, errPath := enforcer.determineStatus(ctx, stat, minVersion)
-
-	assert.Equal(t, schema.NodeStatusInitializing, status)
-	assert.Equal(t, "", errPath)
-}
-
-func Test_NodeStatusOutdatedReturnsOutdated(t *testing.T) {
-	t.Parallel()
-
-	ctx := context.Background()
-
-	mockClient := new(MockHTTPClient)
-	mockClient.On("FetchWithMethod", mock.Anything, "http://localhost:8080/workers_status").Return(io.NopCloser(bytes.NewReader([]byte(workerStatusNodeIndexing))), nil)
-	mockClient.On("FetchWithMethod", mock.Anything, "http://localhost:8080/info").Return(io.NopCloser(bytes.NewReader([]byte(nodeInfo))), nil)
-
-	enforcer := &SimpleEnforcer{
-		httpClient: mockClient,
-	}
-	stat := &schema.Stat{
-		Address:     common.Address{0},
-		Endpoint:    "http://localhost:8080",
-		AccessToken: "token",
-		Epoch:       1,
-		Status:      schema.NodeStatusOutdated,
-	}
-
-	minVersion, _ := version.NewVersion("1.1.0")
-
-	status, errPath := enforcer.determineStatus(ctx, stat, minVersion)
-
-	assert.Equal(t, schema.NodeStatusOutdated, status)
-	assert.Equal(t, "", errPath)
-}
-
-func Test_NodeStatusOutdatedReturnsRegistered(t *testing.T) {
-	t.Parallel()
-
-	ctx := context.Background()
-
-	mockClient := new(MockHTTPClient)
-	mockClient.On("FetchWithMethod", mock.Anything, "http://localhost:8080/workers_status").Return(io.NopCloser(bytes.NewReader([]byte(workerStatusNodeUnhealthy))), nil)
-	mockClient.On("FetchWithMethod", mock.Anything, "http://localhost:8080/info").Return(io.NopCloser(bytes.NewReader([]byte(nodeInfo))), nil)
-
-	enforcer := &SimpleEnforcer{
-		httpClient: mockClient,
-	}
-	stat := &schema.Stat{
-		Address:     common.Address{0},
-		Endpoint:    "http://localhost:8080",
-		AccessToken: "token",
-		Epoch:       1,
-		Status:      schema.NodeStatusOutdated,
-	}
-
-	minVersion, _ := version.NewVersion("1.0.0")
-
-	status, errPath := enforcer.determineStatus(ctx, stat, minVersion)
-
-	assert.Equal(t, schema.NodeStatusRegistered, status)
-	assert.Equal(t, "", errPath)
-}
-
-func Test_NodeStatusOutdatedReturnsInitializing(t *testing.T) {
-	t.Parallel()
-
-	ctx := context.Background()
-
-	mockClient := new(MockHTTPClient)
-	mockClient.On("FetchWithMethod", mock.Anything, "http://localhost:8080/workers_status").Return(io.NopCloser(bytes.NewReader([]byte(workerStatusNodeIndexing))), nil)
-	mockClient.On("FetchWithMethod", mock.Anything, "http://localhost:8080/info").Return(io.NopCloser(bytes.NewReader([]byte(nodeInfo))), nil)
-
-	enforcer := &SimpleEnforcer{
-		httpClient: mockClient,
-	}
-	stat := &schema.Stat{
-		Address:     common.Address{0},
-		Endpoint:    "http://localhost:8080",
-		AccessToken: "token",
-		Epoch:       1,
-		Status:      schema.NodeStatusOutdated,
-	}
-
-	minVersion, _ := version.NewVersion("1.0.0")
-
-	status, errPath := enforcer.determineStatus(ctx, stat, minVersion)
-
-	assert.Equal(t, schema.NodeStatusInitializing, status)
-	assert.Equal(t, "", errPath)
-}
-
-func Test_NodeStatusInitializingReturnsOutdated(t *testing.T) {
-	t.Parallel()
-
-	ctx := context.Background()
-
-	mockClient := new(MockHTTPClient)
-	mockClient.On("FetchWithMethod", mock.Anything, "http://localhost:8080/workers_status").Return(io.NopCloser(bytes.NewReader([]byte(workerStatusNodeIndexing))), nil)
-	mockClient.On("FetchWithMethod", mock.Anything, "http://localhost:8080/info").Return(io.NopCloser(bytes.NewReader([]byte(nodeInfo))), nil)
-
-	enforcer := &SimpleEnforcer{
-		httpClient: mockClient,
-	}
-	stat := &schema.Stat{
-		Address:     common.Address{0},
-		Endpoint:    "http://localhost:8080",
-		AccessToken: "token",
-		Epoch:       1,
-		Status:      schema.NodeStatusInitializing,
-	}
-
-	minVersion, _ := version.NewVersion("1.1.0")
-
-	status, errPath := enforcer.determineStatus(ctx, stat, minVersion)
-
-	assert.Equal(t, schema.NodeStatusOutdated, status)
-	assert.Equal(t, "", errPath)
-}
-
-func Test_NodeStatusInitializingReturnsRegistered(t *testing.T) {
-	t.Parallel()
-
-	ctx := context.Background()
-
-	mockClient := new(MockHTTPClient)
-	mockClient.On("FetchWithMethod", mock.Anything, "http://localhost:8080/workers_status").Return(io.NopCloser(bytes.NewReader([]byte(workerStatusNodeUnhealthy))), nil)
-	mockClient.On("FetchWithMethod", mock.Anything, "http://localhost:8080/info").Return(io.NopCloser(bytes.NewReader([]byte(nodeInfo))), nil)
-
-	enforcer := &SimpleEnforcer{
-		httpClient: mockClient,
-	}
-	stat := &schema.Stat{
-		Address:     common.Address{0},
-		Endpoint:    "http://localhost:8080",
-		AccessToken: "token",
-		Epoch:       1,
-		Status:      schema.NodeStatusInitializing,
-	}
-
-	minVersion, _ := version.NewVersion("1.0.0")
-
-	status, errPath := enforcer.determineStatus(ctx, stat, minVersion)
-
-	assert.Equal(t, schema.NodeStatusRegistered, status)
-	assert.Equal(t, "", errPath)
-}
-
-func Test_NodeStatusInitializingReturnsInitializing(t *testing.T) {
-	t.Parallel()
-
-	ctx := context.Background()
-
-	mockClient := new(MockHTTPClient)
-	mockClient.On("FetchWithMethod", mock.Anything, "http://localhost:8080/workers_status").Return(io.NopCloser(bytes.NewReader([]byte(workerStatusNodeIndexing))), nil)
-	mockClient.On("FetchWithMethod", mock.Anything, "http://localhost:8080/info").Return(io.NopCloser(bytes.NewReader([]byte(nodeInfo))), nil)
-
-	enforcer := &SimpleEnforcer{
-		httpClient: mockClient,
-	}
-	stat := &schema.Stat{
-		Address:     common.Address{0},
-		Endpoint:    "http://localhost:8080",
-		AccessToken: "token",
-		Epoch:       1,
-		Status:      schema.NodeStatusInitializing,
-	}
-
-	minVersion, _ := version.NewVersion("1.0.0")
-
-	status, errPath := enforcer.determineStatus(ctx, stat, minVersion)
-
-	assert.Equal(t, schema.NodeStatusInitializing, status)
-	assert.Equal(t, "", errPath)
+	return mockClient
 }
