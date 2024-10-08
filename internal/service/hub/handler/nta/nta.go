38ce4caf456171eee29f938335bc42fd066bcb69
brucexc <108378464+brucexc@users.noreply.github.com>
2024-10-07 22:14:30 -0400

    feat: add total supply and tvl api (#394)

--- internal/service/hub/handler/nta/nta.go
+++ internal/service/hub/handler/nta/nta.go
@@ -5,21 +5,29 @@ import (
 	"math/big"
 	"net/url"
 
+	"github.com/ethereum-optimism/optimism/op-bindings/bindings"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/labstack/echo/v4"
 	"github.com/rss3-network/global-indexer/common/geolite2"
 	"github.com/rss3-network/global-indexer/common/httputil"
 	"github.com/rss3-network/global-indexer/contract/l2"
 	"github.com/rss3-network/global-indexer/internal/cache"
+	"github.com/rss3-network/global-indexer/internal/config"
 	"github.com/rss3-network/global-indexer/internal/database"
 )
 
 type NTA struct {
-	databaseClient        database.Client
-	stakingContract       *l2.StakingV2MulticallClient
-	networkParamsContract *l2.NetworkParams
-	geoLite2              *geolite2.Client
-	cacheClient           cache.Client
-	httpClient            httputil.Client
+	databaseClient          database.Client
+	stakingContract         *l2.StakingV2MulticallClient
+	networkParamsContract   *l2.NetworkParams
+	contractGovernanceToken *bindings.GovernanceToken
+	geoLite2                *geolite2.Client
+	cacheClient             cache.Client
+	httpClient              httputil.Client
+	erc20TokenMap           map[common.Address]*bindings.GovernanceToken
+	configFile              *config.File
+	chainL1ID               uint64
+	chainL2ID               uint64
 }
 
 var MinDeposit = new(big.Int).Mul(big.NewInt(10000), big.NewInt(1e18))
@@ -31,13 +39,18 @@ func (n *NTA) baseURL(c echo.Context) url.URL {
 	}
 }
 
-func NewNTA(_ context.Context, databaseClient database.Client, stakingContract *l2.StakingV2MulticallClient, networkParamsContract *l2.NetworkParams, geoLite2 *geolite2.Client, cacheClient cache.Client, httpClient httputil.Client) *NTA {
+func NewNTA(_ context.Context, configFile *config.File, databaseClient database.Client, stakingContract *l2.StakingV2MulticallClient, networkParamsContract *l2.NetworkParams, contractGovernanceToken *bindings.GovernanceToken, geoLite2 *geolite2.Client, cacheClient cache.Client, httpClient httputil.Client, erc20TokenMap map[common.Address]*bindings.GovernanceToken, chainL1ID, chainL2ID uint64) *NTA {
 	return &NTA{
-		databaseClient:        databaseClient,
-		stakingContract:       stakingContract,
-		networkParamsContract: networkParamsContract,
-		geoLite2:              geoLite2,
-		cacheClient:           cacheClient,
-		httpClient:            httpClient,
+		databaseClient:          databaseClient,
+		stakingContract:         stakingContract,
+		networkParamsContract:   networkParamsContract,
+		contractGovernanceToken: contractGovernanceToken,
+		geoLite2:                geoLite2,
+		cacheClient:             cacheClient,
+		httpClient:              httpClient,
+		erc20TokenMap:           erc20TokenMap,
+		configFile:              configFile,
+		chainL1ID:               chainL1ID,
+		chainL2ID:               chainL2ID,
 	}
 }
--- internal/service/hub/hub.go
+++ internal/service/hub/hub.go
@@ -5,12 +5,15 @@ import (
 	"fmt"
 	"math/big"
 
+	"github.com/ethereum-optimism/optimism/op-bindings/bindings"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
 	"github.com/redis/go-redis/v9"
 	"github.com/rss3-network/global-indexer/common/geolite2"
 	"github.com/rss3-network/global-indexer/common/httputil"
 	"github.com/rss3-network/global-indexer/common/txmgr"
+	"github.com/rss3-network/global-indexer/contract/l1"
 	"github.com/rss3-network/global-indexer/contract/l2"
 	"github.com/rss3-network/global-indexer/internal/cache"
 	"github.com/rss3-network/global-indexer/internal/client/ethereum"
@@ -20,6 +23,7 @@ import (
 	"github.com/rss3-network/global-indexer/internal/nameresolver"
 	"github.com/rss3-network/global-indexer/internal/service/hub/handler/dsl"
 	"github.com/rss3-network/global-indexer/internal/service/hub/handler/nta"
+	"github.com/samber/lo"
 	"github.com/spf13/viper"
 )
 
@@ -42,22 +46,22 @@ func (v *Validator) Validate(i interface{}) error {
 	return v.validate.Struct(i)
 }
 
-func NewHub(ctx context.Context, databaseClient database.Client, redisClient *redis.Client, ethereumMultiChainClient *ethereum.MultiChainClient, geoLite2 *geolite2.Client, nameService *nameresolver.NameResolver, httpClient httputil.Client, txManager *txmgr.SimpleTxManager, settlerConfig *config.Settler) (*Hub, error) {
-	chainID := viper.GetUint64(flag.KeyChainIDL2)
+func NewHub(ctx context.Context, databaseClient database.Client, redisClient *redis.Client, ethereumMultiChainClient *ethereum.MultiChainClient, geoLite2 *geolite2.Client, nameService *nameresolver.NameResolver, httpClient httputil.Client, txManager *txmgr.SimpleTxManager, config *config.File) (*Hub, error) {
+	chainL2ID := viper.GetUint64(flag.KeyChainIDL2)
 
-	ethereumClient, err := ethereumMultiChainClient.Get(chainID)
+	ethereumClient, err := ethereumMultiChainClient.Get(chainL2ID)
 	if err != nil {
 		return nil, fmt.Errorf("get ethereum client: %w", err)
 	}
 
-	stakingV2MulticallClient, err := l2.NewStakingV2MulticallClient(chainID, ethereumClient)
+	stakingV2MulticallClient, err := l2.NewStakingV2MulticallClient(chainL2ID, ethereumClient)
 	if err != nil {
 		return nil, fmt.Errorf("new staking v2 multicall client: %w", err)
 	}
 
-	contractAddresses := l2.ContractMap[chainID]
+	contractAddresses := l2.ContractMap[chainL2ID]
 	if contractAddresses == nil {
-		return nil, fmt.Errorf("contract address not found for chain id: %d", chainID)
+		return nil, fmt.Errorf("contract address not found for chain id: %d", chainL2ID)
 	}
 
 	networkParamsContract, err := l2.NewNetworkParams(contractAddresses.AddressNetworkParamsProxy, ethereumClient)
@@ -67,13 +71,30 @@ func NewHub(ctx context.Context, databaseClient database.Client, redisClient *re
 
 	cacheClient := cache.New(redisClient)
 
-	dslService, err := dsl.NewDSL(ctx, databaseClient, cacheClient, nameService, stakingV2MulticallClient, networkParamsContract, httpClient, txManager, settlerConfig, new(big.Int).SetUint64(chainID))
+	dslService, err := dsl.NewDSL(ctx, databaseClient, cacheClient, nameService, stakingV2MulticallClient, networkParamsContract, httpClient, txManager, config.Settler, new(big.Int).SetUint64(chainL2ID))
 	if err != nil {
 		return nil, fmt.Errorf("new dsl: %w", err)
 	}
 
+	contractGovernanceToken := lo.Must(bindings.NewGovernanceToken(l2.AddressGovernanceTokenProxy, ethereumClient))
+
+	chainL1ID := viper.GetUint64(flag.KeyChainIDL1)
+
+	ethereumL1Client, err := ethereumMultiChainClient.Get(chainL1ID)
+	if err != nil {
+		return nil, fmt.Errorf("get ethereum l1 client: %w", err)
+	}
+
+	erc20TokenMap := map[common.Address]*bindings.GovernanceToken{
+		l1.ContractMap[chainL1ID].AddressGovernanceTokenProxy: lo.Must(bindings.NewGovernanceToken(l1.ContractMap[chainL1ID].AddressGovernanceTokenProxy, ethereumL1Client)),
+		l1.ContractMap[chainL1ID].AddressUSDCToken:            lo.Must(bindings.NewGovernanceToken(l1.ContractMap[chainL1ID].AddressUSDCToken, ethereumL1Client)),
+		l1.ContractMap[chainL1ID].AddressUSDTToken:            lo.Must(bindings.NewGovernanceToken(l1.ContractMap[chainL1ID].AddressUSDTToken, ethereumL1Client)),
+		l1.ContractMap[chainL1ID].AddressWETHToken:            lo.Must(bindings.NewGovernanceToken(l1.ContractMap[chainL1ID].AddressWETHToken, ethereumL1Client)),
+		l2.ContractMap[chainL2ID].AddressPowerToken:           lo.Must(bindings.NewGovernanceToken(l2.ContractMap[chainL2ID].AddressPowerToken, ethereumClient)),
+	}
+
 	return &Hub{
 		dsl: dslService,
-		nta: nta.NewNTA(ctx, databaseClient, stakingV2MulticallClient, networkParamsContract, geoLite2, cacheClient, httpClient),
+		nta: nta.NewNTA(ctx, config, databaseClient, stakingV2MulticallClient, networkParamsContract, contractGovernanceToken, geoLite2, cacheClient, httpClient, erc20TokenMap, chainL1ID, chainL2ID),
 	}, nil
 }
