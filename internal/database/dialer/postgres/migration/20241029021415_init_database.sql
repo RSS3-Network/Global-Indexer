7ca8eb8f10b33adff95ac9d4f4ef690c288e7623
brucexc <brucexc@rss3.io>
2024-10-29 14:47:08 -0400

    chore: standardize the index name using the format: schema_table_field

--- internal/database/dialer/postgres/migration/20241029021415_init_database.sql
+++ internal/database/dialer/postgres/migration/20241029021415_init_database.sql
@@ -19,9 +19,9 @@ create table if not exists "bridge"."events"
     constraint pk_bridge_events primary key (transaction_hash, block_hash)
 );
 
-create index if not exists "idx_id" on "bridge"."events" (id);
+create index if not exists "idx_bridge_events_id" on "bridge"."events" (id);
 
-create index if not exists "idx_events_chain_id_block_number" on "bridge"."events" (chain_id, block_number);
+create index if not exists "idx_bridge_events_chain_id_block_number" on "bridge"."events" (chain_id, block_number);
 
 create table if not exists "bridge"."transactions"
 (
@@ -41,15 +41,15 @@ create table if not exists "bridge"."transactions"
     constraint pk_bridge_transactions primary key (id, type)
 );
 
-create index if not exists "idx_transactions_sender" on "bridge"."transactions" (sender);
+create index if not exists "idx_bridge_transactions_sender" on "bridge"."transactions" (sender);
 
-create index if not exists "idx_transactions_address" on "bridge"."transactions" (sender, receiver);
+create index if not exists "idx_bridge_transactions_sender_receiver" on "bridge"."transactions" (sender, receiver);
 
-create index if not exists "idx_transactions_receiver" on "bridge"."transactions" (receiver);
+create index if not exists "idx_bridge_transactions_receiver" on "bridge"."transactions" (receiver);
 
-create index if not exists "idx_transactions_chain_id_block_number" on "bridge"."transactions" (chain_id, block_number);
+create index if not exists "idx_bridge_transactions_chain_id_block_number" on "bridge"."transactions" (chain_id, block_number);
 
-create index if not exists "idx_transactions_order" on "bridge"."transactions" (block_timestamp desc, block_number desc, transaction_index desc);
+create index if not exists "idx_bridge_transactions_order" on "bridge"."transactions" (block_timestamp desc, block_number desc, transaction_index desc);
 
 -- epoch schema
 create schema if not exists "epoch";
@@ -64,7 +64,7 @@ create table if not exists "epoch"."apy_snapshots"
     constraint pk_epoch_apy_snapshots primary key (epoch_id)
 );
 
-create index if not exists "apy_snapshots_date_idx" on "epoch"."apy_snapshots" (date);
+create index if not exists "idx_epoch_apy_snapshots_date" on "epoch"."apy_snapshots" (date);
 
 -- node schema
 create schema if not exists "node";
@@ -96,15 +96,15 @@ create table if not exists "node"."events"
     constraint pk_node_events primary key (transaction_hash, transaction_index, log_index)
 );
 
-create index if not exists "events_index_node_id" on "node"."events" (node_id);
+create index if not exists "idx_node_events_node_id" on "node"."events" (node_id);
 
-create index if not exists "events_index_address" on "node"."events" (address_from, address_to);
+create index if not exists "idx_node_events_address_from_address_to" on "node"."events" (address_from, address_to);
 
-create index if not exists "events_index_address_type" on "node"."events" (address_from, type);
+create index if not exists "idx_node_events_address_from_type" on "node"."events" (address_from, type);
 
-create index if not exists "events_index_block_number" on "node"."events" (block_number desc, transaction_index desc, log_index desc);
+create index if not exists "idx_node_events_block_number_transaction_index_log_index" on "node"."events" (block_number desc, transaction_index desc, log_index desc);
 
-create index if not exists "idx_events_block_number" on "node"."events" (block_number);
+create index if not exists "idx_node_events_block_number" on "node"."events" (block_number);
 
 create sequence if not exists "node"."operator_profit_snapshots_id_seq" minvalue 0;
 
@@ -120,13 +120,13 @@ create table if not exists "node"."operator_profit_snapshots"
     constraint pk_node_operator_profit_snapshots primary key (operator, epoch_id)
 );
 
-create index if not exists "operator_profit_snapshots_operation_pool_idx" on "node"."operator_profit_snapshots" (operation_pool desc);
+create index if not exists "idx_node_operator_profit_snapshots_operation_pool" on "node"."operator_profit_snapshots" (operation_pool desc);
 
-create index if not exists "operator_profit_snapshots_epoch_id_idx" on "node"."operator_profit_snapshots" (epoch_id desc);
+create index if not exists "idx_node_operator_profit_snapshots_epoch_id" on "node"."operator_profit_snapshots" (epoch_id desc);
 
-create index if not exists "operator_profit_snapshots_id_idx" on "node"."operator_profit_snapshots" (id desc);
+create index if not exists "idx_node_operator_profit_snapshots_id" on "node"."operator_profit_snapshots" (id desc);
 
-create index if not exists "operator_profit_snapshots_date_idx" on "node"."operator_profit_snapshots" (date);
+create index if not exists "idx_node_operator_profit_snapshots_date" on "node"."operator_profit_snapshots" (date);
 
 create sequence if not exists "node"."apy_snapshots_id_seq" minvalue 0;
 
@@ -142,9 +142,9 @@ create table if not exists "node"."apy_snapshots"
     constraint pk_node_apy_snapshots primary key (node_address, epoch_id)
 );
 
-create index if not exists "apy_snapshots_date_idx" on "node"."apy_snapshots" (date);
+create index if not exists "idx_node_apy_snapshots_date" on "node"."apy_snapshots" (date);
 
-create index if not exists "apy_snapshots_epoch_id_idx" on "node"."apy_snapshots" (epoch_id desc, id desc);
+create index if not exists "idx_node_apy_snapshots_epoch_id_id" on "node"."apy_snapshots" (epoch_id desc, id desc);
 
 -- stake schema
 create schema if not exists "stake";
@@ -164,15 +164,15 @@ create table if not exists "stake"."transactions"
     constraint pk_stake_transactions primary key (id, type)
 );
 
-create index if not exists "idx_transactions_block_number" on "stake"."transactions" (block_number);
+create index if not exists "idx_stake_transactions_block_number" on "stake"."transactions" (block_number);
 
-create index if not exists "idx_transactions_order" on "stake"."transactions" (block_timestamp desc, block_number desc, transaction_index desc);
+create index if not exists "idx_stake_transactions_order" on "stake"."transactions" (block_timestamp desc, block_number desc, transaction_index desc);
 
-create index if not exists "idx_transactions_node" on "stake"."transactions" (node);
+create index if not exists "idx_stake_transactions_node" on "stake"."transactions" (node);
 
-create index if not exists "idx_transactions_address" on "stake"."transactions" ("user", node);
+create index if not exists "idx_stake_transactions_user_node" on "stake"."transactions" ("user", node);
 
-create index if not exists "idx_transactions_user" on "stake"."transactions" ("user");
+create index if not exists "idx_stake_transactions_user" on "stake"."transactions" ("user");
 
 create table if not exists "stake"."events"
 (
@@ -190,11 +190,11 @@ create table if not exists "stake"."events"
     constraint pk_stake_events primary key (transaction_hash, log_index, id)
 );
 
-create index if not exists "idx_id" on "stake"."events" (id);
+create index if not exists "idx_stake_events_id" on "stake"."events" (id);
 
-create index if not exists "idx_events_order" on "stake"."events" (block_timestamp desc, block_number desc, transaction_index desc);
+create index if not exists "idx_stake_events_order" on "stake"."events" (block_timestamp desc, block_number desc, transaction_index desc);
 
-create index if not exists "idx_events_block_number" on "stake"."events" (block_number);
+create index if not exists "idx_stake_events_block_number" on "stake"."events" (block_number);
 
 create table if not exists "stake"."chips"
 (
@@ -209,13 +209,13 @@ create table if not exists "stake"."chips"
     constraint pk_stake_chips primary key (id)
 );
 
-create index if not exists "idx_owner_node_value_finalized" on "stake"."chips" (owner, node, value, finalized);
+create index if not exists "idx_stake_chips_owner_node_value_finalized" on "stake"."chips" (owner, node, value, finalized);
 
-create index if not exists "idx_block_number" on "stake"."chips" (block_number);
+create index if not exists "idx_stake_chips_block_number" on "stake"."chips" (block_number);
 
-create index if not exists "idx_node" on "stake"."chips" (node);
+create index if not exists "idx_stake_chips_node" on "stake"."chips" (node);
 
-create index if not exists "idx_owner" on "stake"."chips" (owner);
+create index if not exists "idx_stake_chips_owner" on "stake"."chips" (owner);
 
 create table if not exists "stake"."count_snapshots"
 (
@@ -239,15 +239,15 @@ create table if not exists "stake"."profit_snapshots"
     constraint pk_stake_profit_snapshots primary key (owner_address, epoch_id)
 );
 
-create index if not exists "profit_snapshots_date_idx" on "stake"."profit_snapshots" (date);
+create index if not exists "idx_stake_profit_snapshots_date" on "stake"."profit_snapshots" (date);
 
-create index if not exists "profit_snapshots_total_chip_amounts_idx" on "stake"."profit_snapshots" (total_chip_amounts desc);
+create index if not exists "idx_stake_profit_snapshots_total_chip_amounts" on "stake"."profit_snapshots" (total_chip_amounts desc);
 
-create index if not exists "profit_snapshots_epoch_id_idx" on "stake"."profit_snapshots" (epoch_id desc, id desc);
+create index if not exists "idx_stake_profit_snapshots_epoch_id_id" on "stake"."profit_snapshots" (epoch_id desc, id desc);
 
-create index if not exists "profit_snapshots_id_idx" on "stake"."profit_snapshots" (id);
+create index if not exists "idx_stake_profit_snapshots_id" on "stake"."profit_snapshots" (id);
 
-create index if not exists "profit_snapshots_total_chip_values_idx" on "stake"."profit_snapshots" (total_chip_values desc);
+create index if not exists "idx_stake_profit_snapshots_total_chip_values" on "stake"."profit_snapshots" (total_chip_values desc);
 
 create view "stake"."stakings"(staker, node, count, value) as
 SELECT owner AS staker, node, count(*) AS count, sum(value) AS value
@@ -284,13 +284,13 @@ create table if not exists "node_info"
     constraint idx_endpoint_unique unique (endpoint)
 );
 
-create index if not exists "idx_is_public" on "node_info" (is_public_good asc, created_at desc);
+create index if not exists "idx_node_info_is_public" on "node_info" (is_public_good asc, created_at desc);
 
-create index if not exists "idx_last_heartbeat_timestamp" on "node_info" (last_heartbeat_timestamp);
+create index if not exists "idx_node_info_last_heartbeat_timestamp" on "node_info" (last_heartbeat_timestamp);
 
-create index if not exists "idx_status" on "node_info" (status);
+create index if not exists "idx_node_info_status" on "node_info" (status);
 
-create index if not exists "idx_created_at" on "node_info" (address asc, created_at desc);
+create index if not exists "idx_node_info_address_created_at" on "node_info" (address asc, created_at desc);
 
 create index if not exists "idx_node_info_version" on "node_info" (version);
 
@@ -329,15 +329,15 @@ create table if not exists "node_stat"
     constraint pk_node_stat primary key (address)
 );
 
-create index if not exists "idx_indexes_created_at" on "node_stat" (created_at);
+create index if not exists "idx_node_stat_created_at" on "node_stat" (created_at);
 
-create index if not exists "idx_indexes_epoch_invalid_request_count" on "node_stat" (epoch_invalid_request_count);
+create index if not exists "idx_node_stat_epoch_invalid_request_count" on "node_stat" (epoch_invalid_request_count);
 
-create index if not exists "idx_indexes_points" on "node_stat" (points desc);
+create index if not exists "idx_node_stat_points" on "node_stat" (points desc);
 
-create index if not exists "idx_indexes_is_full_node" on "node_stat" (is_full_node, points desc);
+create index if not exists "idx_node_stat_is_full_node_points" on "node_stat" (is_full_node, points desc);
 
-create index if not exists "idx_indexes_is_rss_node" on "node_stat" (is_rss_node, points desc);
+create index if not exists "idx_node_stat_is_rss_node_points" on "node_stat" (is_rss_node, points desc);
 
 create table if not exists "average_tax_rate_submissions"
 (
@@ -350,11 +350,11 @@ create table if not exists "average_tax_rate_submissions"
     constraint pk_average_tax_rate_submissions primary key (epoch_id)
 );
 
-create index if not exists "average_tax_rate_submissions_transaction_hash_idx" on "average_tax_rate_submissions" (transaction_hash);
+create index if not exists "idx_average_tax_rate_submissions_transaction_hash" on "average_tax_rate_submissions" (transaction_hash);
 
-create index if not exists "average_tax_rate_submissions_id_idx" on "average_tax_rate_submissions" (id desc);
+create index if not exists "idx_average_tax_rate_submissions_id" on "average_tax_rate_submissions" (id desc);
 
-create index if not exists "average_tax_rate_submissions_epoch_id_idx" on "average_tax_rate_submissions" (epoch_id desc);
+create index if not exists "idx_average_tax_rate_submissions_epoch_id" on "average_tax_rate_submissions" (epoch_id desc);
 
 create table if not exists "epoch"
 (
@@ -376,9 +376,9 @@ create table if not exists "epoch"
     constraint pk_epoch primary key (transaction_hash)
 );
 
-create index if not exists "idx_timestamp" on "epoch" (start_timestamp desc, end_timestamp desc);
+create index if not exists "idx_epoch_start_timestamp_end_timestamp" on "epoch" (start_timestamp desc, end_timestamp desc);
 
-create index if not exists "idx_epoch_id" on "epoch" (id desc, block_number desc, transaction_index desc);
+create index if not exists "idx_epoch_id_block_number_transaction_index" on "epoch" (id desc, block_number desc, transaction_index desc);
 
 create table if not exists "epoch_trigger"
 (
@@ -392,7 +392,7 @@ create table if not exists "epoch_trigger"
 
 create index if not exists "idx_epoch_trigger_epoch_id" on "epoch_trigger" (epoch_id);
 
-create index if not exists "idx_created_at" on "epoch_trigger" (created_at);
+create index if not exists "idx_epoch_trigger_created_at" on "epoch_trigger" (created_at);
 
 create table if not exists "node_invalid_response"
 (
@@ -409,11 +409,11 @@ create table if not exists "node_invalid_response"
     constraint pk_node_invalid_response primary key (id)
 );
 
-create index if not exists "idx_node" on "node_invalid_response" (node asc, created_at desc);
+create index if not exists "idx_node_invalid_response_node_created_at" on "node_invalid_response" (node asc, created_at desc);
 
-create index if not exists "idx_request" on "node_invalid_response" (request asc, created_at desc);
+create index if not exists "idx_node_invalid_response_request_created_at" on "node_invalid_response" (request asc, created_at desc);
 
-create index if not exists "idx_type" on "node_invalid_response" (type asc, created_at desc);
+create index if not exists "idx_node_invalid_response_type_created_at" on "node_invalid_response" (type asc, created_at desc);
 
 create index if not exists "idx_node_invalid_response_epoch_id" on "node_invalid_response" (epoch_id desc);
 
@@ -432,9 +432,9 @@ create table if not exists "node_reward_record"
     constraint pk_epoch_item primary key (transaction_hash, index)
 );
 
-create index if not exists "idx_epoch_item_node_address" on "node_reward_record" (node_address);
+create index if not exists "idx_node_reward_record_node_address" on "node_reward_record" (node_address);
 
-create index if not exists "idx_epoch_item_epoch_id" on "node_reward_record" (epoch_id);
+create index if not exists "idx_node_reward_record_epoch_id" on "node_reward_record" (epoch_id);
 
 create table if not exists "node_worker"
 (
